5f0d0354c2d2c6b7ae1f23e7792f00bad2cff503
felix-oq <51856713+felix-oq@users.noreply.github.com>
2022-10-07 10:48:51 +0200

    Setup initial Langium project

---
+++ src/language-server/main.ts
@@ -0,0 +1,13 @@
+import { startLanguageServer } from 'langium';
+import { NodeFileSystem } from 'langium/node';
+import { createConnection, ProposedFeatures } from 'vscode-languageserver/node';
+import { createOpenDataLanguageServices } from './open-data-language-module';
+
+// Create a connection to the client
+const connection = createConnection(ProposedFeatures.all);
+
+// Inject the shared services and language-specific services
+const { shared } = createOpenDataLanguageServices({ connection, ...NodeFileSystem });
+
+// Start the language server with the shared services
+startLanguageServer(shared);
---
+++ src/language-server/open-data-language-validator.ts
@@ -0,0 +1,33 @@
+import { ValidationAcceptor, ValidationChecks, ValidationRegistry } from 'langium';
+import { OpenDataLanguageAstType, Person } from './generated/ast';
+import type { OpenDataLanguageServices } from './open-data-language-module';
+
+/**
+ * Registry for validation checks.
+ */
+export class OpenDataLanguageValidationRegistry extends ValidationRegistry {
+    constructor(services: OpenDataLanguageServices) {
+        super(services);
+        const validator = services.validation.OpenDataLanguageValidator;
+        const checks: ValidationChecks<OpenDataLanguageAstType> = {
+            Person: validator.checkPersonStartsWithCapital
+        };
+        this.register(checks, validator);
+    }
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class OpenDataLanguageValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
