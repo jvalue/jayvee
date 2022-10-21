287d97ff0cfdf69146ec6a23ecaa2e4923682cb5
felix-oq <51856713+felix-oq@users.noreply.github.com>
2022-10-21 14:57:27 +0200

    Setup interpreter command line interface

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,34 +1,8 @@
-import chalk from 'chalk';
 import { Command } from 'commander';
-import { NodeFileSystem } from 'langium/node';
 
-import { Model } from '../language-server/generated/ast';
 import { OpenDataLanguageLanguageMetaData } from '../language-server/generated/module';
-import { createOpenDataLanguageServices } from '../language-server/open-data-language-module';
 
-import { extractAstNode } from './cli-util';
-import { generateJavaScript } from './generator';
-
-export const generateAction = async (
-  fileName: string,
-  opts: GenerateOptions,
-): Promise<void> => {
-  const services =
-    createOpenDataLanguageServices(NodeFileSystem).OpenDataLanguage;
-  const model = await extractAstNode<Model>(fileName, services);
-  const generatedFilePath = generateJavaScript(
-    model,
-    fileName,
-    opts.destination,
-  );
-  console.log(
-    chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`),
-  );
-};
-
-export interface GenerateOptions {
-  destination?: string;
-}
+import { runAction } from './interpreter';
 
 export default function (): void {
   const program = new Command();
@@ -36,16 +10,13 @@ export default function (): void {
   const fileExtensions =
     OpenDataLanguageLanguageMetaData.fileExtensions.join(', ');
   program
-    .command('generate')
+    .command('run')
     .argument(
       '<file>',
       `source file (possible file extensions: ${fileExtensions})`,
     )
-    .option('-d, --destination <dir>', 'destination directory of generating')
-    .description(
-      'generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file',
-    )
-    .action(generateAction);
+    .description('run the pipeline model in the source file')
+    .action(runAction);
 
   program.parse(process.argv);
 }
