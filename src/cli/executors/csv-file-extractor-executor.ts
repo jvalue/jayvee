4fb74c1c6efd1c28791d076f0eb49d426b566dc2
Georg Schwarz <dev@georg-schwarz.com>
2022-10-25 11:44:52 +0200

    Implement fetch part of CsvFileExtractorExecutor

--- src/cli/executors/csv-file-extractor-executor.ts
+++ src/cli/executors/csv-file-extractor-executor.ts
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch'; // TODO: find other library that does not have compilation errors with latest version
+
 import { CSVFileExtractor } from '../../language-server/generated/ast';
 import { Sheet, sheetType, undefinedType } from '../data-types';
 
@@ -12,8 +14,25 @@ export class CSVFileExtractorExecutor extends BlockExecutor<
     super(block, undefinedType, sheetType);
   }
 
-  override execute(): Promise<Sheet> {
-    // TODO #8
+  override async execute(): Promise<Sheet> {
+    // Fetch
+    await this.fetchRawData();
+
+    // Interpret
+    // TODO
+
     return Promise.resolve([['example'], ['csv'], ['table']]);
+  }
+
+  private async fetchRawData(): Promise<string> {
+    const url = this.block.url;
+    const response = await fetch(url);
+    const body = await response.text();
+    if (response.status >= 400) {
+      throw Error(
+        `Error when executing block "${this.block.$type}". HTTP fetch failed with code ${response.status}:\n${body}`,
+      );
+    }
+    return body;
   }
 }
