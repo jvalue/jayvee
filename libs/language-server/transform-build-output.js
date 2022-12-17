8f2269abd99ffe17aa34211afc1ab48049030a57
MBuchalik <dev@martin-buchalik.de>
2022-12-17 17:23:41 +0100

    Add a 'publish' target for the language server and for the monaco editor

--- libs/language-server/transform-build-output.js
+++ libs/language-server/transform-build-output.js
@@ -20,10 +20,35 @@ const languageServerPackageJsonPath = path.join(
 const rawPackageJsonContent = fs
   .readFileSync(languageServerPackageJsonPath)
   .toString();
-const parsedPackageJsonContent = JSON.parse(rawPackageJsonContent);
+let parsedPackageJsonContent = JSON.parse(rawPackageJsonContent);
 
 // Get the "version" field from the root package.json file and set it as the version of our package.
 parsedPackageJsonContent.version = getProjectVersion();
 
+/*
+  Unfortunately, it seems like it is not possible to define a "base" package.json for the executor we are using. 
+  However, we need to set a few fields in the generated package.json.
+  
+  To solve this, we use a custom file (which we call `package-overrides.json`) in order to define fields that shall be set (or overridden) in the generated package.json.
+*/
+const packageOverrides = getPackageOverrides();
+parsedPackageJsonContent = { ...parsedPackageJsonContent, ...packageOverrides };
+
 const prettyPrintedContent = JSON.stringify(parsedPackageJsonContent, null, 2);
 fs.writeFileSync(languageServerPackageJsonPath, prettyPrintedContent);
+
+/**
+ * Get the content of `package-overrides.json`.
+ */
+function getPackageOverrides() {
+  const packageOverridesPath = path.join(
+    projectRootPath,
+    'libs',
+    'language-server',
+    'package-overrides.json',
+  );
+  const fileContent = fs.readFileSync(packageOverridesPath);
+  const parsedFileContent = JSON.parse(fileContent);
+
+  return parsedFileContent;
+}
