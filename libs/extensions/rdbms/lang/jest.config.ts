7801e272a195dba8246000223e7ae66f48dab217
felix-oq <51856713+felix-oq@users.noreply.github.com>
2022-12-21 18:35:51 +0100

    Fix jest path

--- libs/extensions/rdbms/lang/jest.config.ts
+++ libs/extensions/rdbms/lang/jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   displayName: 'extensions-rdbms',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
