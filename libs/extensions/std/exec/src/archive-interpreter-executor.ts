8a64fc1a8a6f672cae1a0653f7fb9ff3a52e42d5
schlingling <schilling.jn@icloud.com>
2023-02-13 19:15:13 +0100

    refactored to async await

--- libs/extensions/std/exec/src/archive-interpreter-executor.ts
+++ libs/extensions/std/exec/src/archive-interpreter-executor.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 
 import * as R from '@jayvee/execution';
-import { BlockExecutor } from '@jayvee/execution';
+import { BlockExecutor, err } from '@jayvee/execution';
 import {
   File,
   FileExtension,
@@ -40,66 +40,47 @@ export class ArchiveInterpreterExecutor extends BlockExecutor<
     });
   }
 
-  private loadZipFileToInMemoryFileSystem(
+  private async loadZipFileToInMemoryFileSystem(
     archiveFile: File,
   ): Promise<R.Result<FileSystem>> {
     this.logger.logDebug(`Loading zip file from binary content`);
-    return new Promise((resolve) => {
+    try {
       const jszip = JSZip();
       const root = new InMemoryFileSystem();
-      jszip
-        .loadAsync(archiveFile.content)
-        .then((archivedObjects) => {
-          archivedObjects.forEach((relPath, archivedObject) => {
-            // Jszip lists directories AND files as objects, we just need the files, when they are put into our filesystem, the corresponding directory gets created by the inmemoryfilesystem
-            if (!archivedObject.dir) {
-              archivedObject
-                .async('arraybuffer')
-                .then((content) => {
-                  const extName = path.extname(archivedObject.name);
-                  const fileName = path.basename(archivedObject.name, extName);
-
-                  // Infer Mimetype from file-extension, if not inferrable, then default to application/octet-stream
-                  const mimeType =
-                    inferMimeTypeFromFileExtension(extName) ||
-                    MimeType.APPLICATION_OCTET_STREAM;
-
-                  // Infer FileExtension from extension in filename, if not inferrable, then default to None
-                  const fileExtension =
-                    inferFileExtensionFromMimeType(extName) ||
-                    FileExtension.NONE;
-
-                  const file: File = {
-                    name: fileName,
-                    extension: fileExtension,
-                    content: content,
-                    mimeType: mimeType,
-                  };
-
-                  // Put files creates dir in root, if dir does not exist
-                  root.putFile(relPath, file);
-                })
-                .catch((error: Error) => {
-                  resolve(
-                    R.err({
-                      message: `Unexpected Error ${error.message} occured during processing of objects inside the archive`,
-                      diagnostic: { node: this.block, property: 'name' },
-                    }),
-                  );
-                });
-            }
-          });
-        })
-        .catch((error: Error) => {
-          resolve(
-            R.err({
-              message: `Unexpected Error ${error.message} occured during load of archivefile`,
-              diagnostic: { node: this.block, property: 'name' },
-            }),
-          );
-        });
-
-      resolve(R.ok(root));
-    });
+      const archivedObjects = await jszip.loadAsync(archiveFile.content);
+      for (const [relPath, archivedObject] of Object.entries(
+        archivedObjects,
+      ) as Array<[string, JSZip.JSZipObject]>) {
+        if (!archivedObject.dir) {
+          const content = await archivedObject.async('arraybuffer');
+          //Ext incl. leading dot
+          const extName = path.extname(archivedObject.name);
+          // Filename without ext and dot
+          const fileName = path.basename(archivedObject.name, extName);
+          const mimeType =
+            inferMimeTypeFromFileExtension(extName) ||
+            MimeType.APPLICATION_OCTET_STREAM;
+          const fileExtension =
+            inferFileExtensionFromMimeType(extName) || FileExtension.NONE;
+          const file: File = {
+            name: fileName,
+            extension: fileExtension,
+            content,
+            mimeType,
+          };
+          root.putFile(relPath, file);
+        }
+      }
+      return Promise.resolve(R.ok(root));
+    } catch (error: unknown) {
+      return Promise.resolve(
+        R.err({
+          message: `Unexpected Error ${
+            err instanceof Error ? err.message : JSON.stringify(err)
+          } occured during processing`,
+          diagnostic: { node: this.block, property: 'name' },
+        }),
+      );
+    }
   }
 }
