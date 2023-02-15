af2bfa126e80c71cb5ced05af518d6ae15ccce41
Georg Schwarz <dev@georg-schwarz.com>
2023-02-15 15:01:04 +0100

    Refactor markdown creation for lsp docs

--- libs/extensions/tabular/lang/src/lib/layout-validator-meta-inf.ts
+++ libs/extensions/tabular/lang/src/lib/layout-validator-meta-inf.ts
@@ -10,33 +10,22 @@ export class LayoutValidatorMetaInformation extends BlockMetaInformation {
     super('LayoutValidator', SHEET_TYPE, TABLE_TYPE, {
       validationLayout: {
         type: AttributeType.LAYOUT,
-        attributeDescription: getValidationLayoutDoc(),
-        exampleUsageDescription: getValidationLayoutExampleDoc(),
+        docs: {
+          description: `Reference to the layout that is used for validation.`,
+          example: docExample,
+        },
       },
     });
     this.docs.description =
       'Valdiates a `Sheet` by a given `layout` to build a `Table`. Rows that do not satisfy the layout are removed.';
-    this.docs.example = `block CarsValidator oftype LayoutValidator {
-  validationLayout: CarsLayout;
-}
-
-layout CarsLayout {
-  // speficy layout
-}`;
+    this.docs.example = docExample;
   }
 }
 
-const getValidationLayoutDoc = () => {
-  return `Reference to the layout that is used for validation.`;
-};
-
-const getValidationLayoutExampleDoc = () => {
-  return `block CarsValidator oftype LayoutValidator {
+const docExample = `block CarsValidator oftype LayoutValidator {
   validationLayout: CarsLayout;
-  // other attributes
 }
 
 layout CarsLayout {
   // speficy layout
 }`;
-};
---
+++ libs/language-server/src/lib/meta-information/markdown-doc-builder.ts
@@ -0,0 +1,61 @@
+export class MarkdownDocBuilder {
+  private markdownTextLines: string[] = [];
+
+  title(title: string): MarkdownDocBuilder {
+    this.markdownTextLines.push(`# ${title}`);
+    return this;
+  }
+
+  blockTitle(blockName: string): MarkdownDocBuilder {
+    return this.title(`Block \`${blockName}\``);
+  }
+
+  attributeTitle(attributeName: string): MarkdownDocBuilder {
+    return this.title(`Attribute \`${attributeName}\``);
+  }
+
+  description(text?: string): MarkdownDocBuilder {
+    if (text === undefined) {
+      return this;
+    }
+    return this.newSection('Description', text);
+  }
+
+  attributes(
+    attributes: Array<[string, string | undefined]>,
+  ): MarkdownDocBuilder {
+    const content = attributes
+      .map(([key, description]) => `- \`${key}\`: ${description ?? ''}`)
+      .join('\n');
+    return this.newSection('Attributes', content);
+  }
+
+  validation(text?: string): MarkdownDocBuilder {
+    if (text === undefined) {
+      return this;
+    }
+    return this.newSection('Validation', text);
+  }
+
+  example(text?: string): MarkdownDocBuilder {
+    if (text === undefined) {
+      return this;
+    }
+    return this.newSection('Example', '```\n' + text + '\n```');
+  }
+
+  newSection(heading: string, content: string): MarkdownDocBuilder {
+    this.markdownTextLines.push(`## ${heading}`);
+    this.markdownTextLines.push(content);
+    return this;
+  }
+
+  newLine(): MarkdownDocBuilder {
+    this.markdownTextLines.push('\n');
+    return this;
+  }
+
+  build(): string {
+    return this.markdownTextLines.join('\n');
+  }
+}
