e9015be960362f9c8f62f61695d1baafd4447adb
Georg Schwarz <dev@georg-schwarz.com>
2023-02-15 14:27:09 +0100

    Add block docs on lsp hover

--- libs/extensions/tabular/lang/src/lib/layout-validator-meta-inf.ts
+++ libs/extensions/tabular/lang/src/lib/layout-validator-meta-inf.ts
@@ -14,11 +14,20 @@ export class LayoutValidatorMetaInformation extends BlockMetaInformation {
         exampleUsageDescription: getValidationLayoutExampleDoc(),
       },
     });
+    this.docs.description =
+      'Valdiates a `Sheet` by a given `layout` to build a `Table`. Rows that do not satisfy the layout are removed.';
+    this.docs.example = `block CarsValidator oftype LayoutValidator {
+  validationLayout: CarsLayout;
+}
+
+layout CarsLayout {
+  // speficy layout
+}`;
   }
 }
 
 const getValidationLayoutDoc = () => {
-  return `Reference to the layout that is used for validation. Rows that do not satisfy the layout are removed.`;
+  return `Reference to the layout that is used for validation.`;
 };
 
 const getValidationLayoutExampleDoc = () => {
