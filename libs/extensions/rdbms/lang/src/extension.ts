504d95345932bbfa1932259bf9d3ad4479439beb
felix-oq <51856713+felix-oq@users.noreply.github.com>
2023-02-15 11:34:00 +0100

    Move table interpreter from rdbms to tabular extension

--- libs/extensions/rdbms/lang/src/extension.ts
+++ libs/extensions/rdbms/lang/src/extension.ts
@@ -7,14 +7,12 @@ import {
   PostgresLoaderMetaInformation,
   SQLiteLoaderMetaInformation,
 } from './lib';
-import { TableInterpreterMetaInformation } from './lib/table-interpreter-meta-inf';
 
 export class RdbmsLangExtension implements JayveeLangExtension {
   getBlockMetaInf(): BlockMetaInformation[] {
     return [
       new PostgresLoaderMetaInformation(),
       new SQLiteLoaderMetaInformation(),
-      new TableInterpreterMetaInformation(),
     ];
   }
 }
