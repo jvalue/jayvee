63854dd567fbaa4824c10ce78855287176c4f408
Georg Schwarz <dev@georg-schwarz.com>
2023-02-16 11:37:28 +0100

    Add documentation for the RDBMS extension

--- libs/extensions/rdbms/lang/src/lib/sqlite-loader-meta-information.ts
+++ libs/extensions/rdbms/lang/src/lib/sqlite-loader-meta-information.ts
@@ -10,10 +10,29 @@ export class SQLiteLoaderMetaInformation extends BlockMetaInformation {
     super('SQLiteLoader', TABLE_TYPE, UNDEFINED_TYPE, {
       table: {
         type: AttributeType.STRING,
+        docs: {
+          description: 'The table to write into.',
+        },
       },
       file: {
         type: AttributeType.STRING,
+        docs: {
+          description: 'The path to the SQLite file that is created.',
+        },
       },
     });
+    this.docs.description = 'Loads a `Table` into a SQLite database sink.';
+    this.docs.examples = [
+      {
+        code: blockExampleUsage,
+        description:
+          'A local SQLite file at the given path is created and filled with the cars table data.',
+      },
+    ];
   }
 }
+
+const blockExampleUsage = `block CarsLoader oftype SQLiteLoader {
+  table: "Cars";
+  file: "./cars.db";
+}`;
