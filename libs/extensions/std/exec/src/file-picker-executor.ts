e26805cf68e354721387edf4e2948f517da59a68
schlingling <schilling.jn@icloud.com>
2023-02-18 13:09:29 +0100

    added skipping of blocks if parent emits null

--- libs/extensions/std/exec/src/file-picker-executor.ts
+++ libs/extensions/std/exec/src/file-picker-executor.ts
@@ -1,13 +1,13 @@
 import * as R from '@jayvee/execution';
 import { BlockExecutor } from '@jayvee/execution';
-import { File, FileSystem, None } from '@jayvee/language-server';
+import { File, FileSystem } from '@jayvee/language-server';
 
-export class FilePickerExecutor extends BlockExecutor<FileSystem, File | None> {
+export class FilePickerExecutor extends BlockExecutor<FileSystem, File | null> {
   constructor() {
     super('FilePicker');
   }
 
-  override execute(fileSystem: FileSystem): Promise<R.Result<File | None>> {
+  override execute(fileSystem: FileSystem): Promise<R.Result<File | null>> {
     const file = fileSystem.getFile(this.getStringAttributeValue('path'));
     return Promise.resolve(R.ok(file));
   }
