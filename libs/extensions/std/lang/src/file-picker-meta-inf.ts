8a832460fb2782ae24093f093930b6c5204eabb7
schlingling <schilling.jn@icloud.com>
2023-02-18 13:20:27 +0100

    added doc to FilePicker

--- libs/extensions/std/lang/src/file-picker-meta-inf.ts
+++ libs/extensions/std/lang/src/file-picker-meta-inf.ts
@@ -21,8 +21,24 @@ export class FilePickerMetaInformation extends BlockMetaInformation {
       {
         path: {
           type: AttributeType.STRING,
+          docs: {
+            description: 'The path of the file to get',
+          },
         },
       },
     );
+
+    this.docs.description = 'Picks an File out of an FileSystem and outputs it';
+    this.docs.examples = [
+      {
+        code: blockExampleUsage,
+        description:
+          'A local SQLite file is created at the given path and filled with table data about cars.',
+      },
+    ];
   }
 }
+
+const blockExampleUsage = `block GtfsFilePicker oftype FilePicker {
+  path: "./Agencies.txt";
+}`;
