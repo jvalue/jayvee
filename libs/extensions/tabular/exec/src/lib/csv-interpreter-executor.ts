d1cb903b0cd2c2ccb0147f58c640a28e8a92c70e
felix-oq <51856713+felix-oq@users.noreply.github.com>
2023-03-20 15:59:26 +0100

    Remove file extension expectations

--- libs/extensions/tabular/exec/src/lib/csv-interpreter-executor.ts
+++ libs/extensions/tabular/exec/src/lib/csv-interpreter-executor.ts
@@ -3,10 +3,10 @@ import { TextDecoder } from 'util';
 import { parseString as parseStringAsCsv } from '@fast-csv/parse';
 import { ParserOptionsArgs } from '@fast-csv/parse/build/src/ParserOptions';
 import * as R from '@jvalue/execution';
-import { BlockExecutor, File, FileExtension, Sheet } from '@jvalue/execution';
+import { BlockExecutor, File, Sheet } from '@jvalue/execution';
 import { IOType } from '@jvalue/language-server';
-import { Either, isLeft } from 'fp-ts/lib/Either';
 import * as E from 'fp-ts/lib/Either';
+import { Either, isLeft } from 'fp-ts/lib/Either';
 
 export class CSVInterpreterExecutor extends BlockExecutor<
   IOType.FILE,
@@ -20,40 +20,25 @@ export class CSVInterpreterExecutor extends BlockExecutor<
   override async execute(file: File): Promise<R.Result<Sheet>> {
     const delimiter = this.getStringAttributeValue('delimiter');
 
-    if (
-      file.extension === FileExtension.TXT ||
-      file.extension === FileExtension.CSV
-    ) {
-      const decoder = new TextDecoder();
-      const csvFile = decoder.decode(file.content);
-      this.logger.logDebug(
-        `Parsing raw data as CSV using delimiter "${delimiter}"`,
-      );
-
-      const csvData = await parseAsCsv(csvFile, delimiter);
-      if (isLeft(csvData)) {
-        return Promise.resolve(
-          R.err({
-            message: `CSV parse failed: ${csvData.left.message}`,
-            diagnostic: { node: this.block, property: 'name' },
-          }),
-        );
-      }
-      const sheet = new Sheet(csvData.right);
-
-      this.logger.logDebug(`Parsing raw data as CSV-sheet successful`);
-      return Promise.resolve(R.ok(sheet));
-    }
-    return Promise.resolve(
-      R.err({
-        message: `Input file's extensions expected to be ${
-          FileExtension.TXT
-        } or ${FileExtension.CSV} but was ${
-          file.extension === FileExtension.NONE ? 'NONE' : file.extension
-        }`,
-        diagnostic: { node: this.block, property: 'name' },
-      }),
+    const decoder = new TextDecoder();
+    const csvFile = decoder.decode(file.content);
+    this.logger.logDebug(
+      `Parsing raw data as CSV using delimiter "${delimiter}"`,
     );
+
+    const csvData = await parseAsCsv(csvFile, delimiter);
+    if (isLeft(csvData)) {
+      return Promise.resolve(
+        R.err({
+          message: `CSV parse failed: ${csvData.left.message}`,
+          diagnostic: { node: this.block, property: 'name' },
+        }),
+      );
+    }
+    const sheet = new Sheet(csvData.right);
+
+    this.logger.logDebug(`Parsing raw data as CSV-sheet successful`);
+    return Promise.resolve(R.ok(sheet));
   }
 }
 
