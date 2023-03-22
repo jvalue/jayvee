cce5e0391471e4488d9f8f84b1166f08c289b2ed
felix-oq <51856713+felix-oq@users.noreply.github.com>
2023-03-22 15:53:52 +0100

    Extract encoding and linebreak properties

--- libs/extensions/std/exec/src/text-file-interpreter-executor.ts
+++ libs/extensions/std/exec/src/text-file-interpreter-executor.ts
@@ -5,6 +5,7 @@ import {
   BinaryFile,
   BlockExecutor,
   BlockExecutorClass,
+  ExecutionContext,
   TextFile,
   implementsStatic,
 } from '@jvalue/execution';
@@ -19,19 +20,33 @@ export class TextFileInterpreterExecutor
   public readonly outputType = IOType.TEXT_FILE;
 
   // eslint-disable-next-line @typescript-eslint/require-await
-  async execute(file: BinaryFile): Promise<R.Result<TextFile>> {
-    const encoding = 'utf-8';
+  async execute(
+    file: BinaryFile,
+    context: ExecutionContext,
+  ): Promise<R.Result<TextFile>> {
+    const encoding = context.getTextPropertyValue('encoding');
+    const lineBreak = context.getRegexPropertyValue('lineBreak');
+
     const decoder = new TextDecoder(encoding);
+    context.logger.logDebug(
+      `Decoding file content using encoding "${encoding}"`,
+    );
     const textContent = decoder.decode(file.content);
 
-    const lines = splitLines(textContent);
+    context.logger.logDebug(
+      `Splitting lines using line break /${lineBreak.source}/`,
+    );
+    const lines = splitLines(textContent, lineBreak);
+    context.logger.logDebug(
+      `Lines were split successfully, the resulting text file has ${lines.length} lines`,
+    );
 
     return R.ok(new TextFile(file.name, file.extension, file.mimeType, lines));
   }
 }
 
-function splitLines(textContent: string): string[] {
-  const lines = textContent.split(/\r?\n/);
+function splitLines(textContent: string, lineBreak: RegExp): string[] {
+  const lines = textContent.split(lineBreak);
 
   // There may be an additional empty line due to the previous splitting
   if (lines[lines.length - 1] === '') {
--- libs/extensions/std/lang/src/text-file-interpreter-meta-inf.ts
+++ libs/extensions/std/lang/src/text-file-interpreter-meta-inf.ts
@@ -1,10 +1,45 @@
-import { BlockMetaInformation, IOType } from '@jvalue/language-server';
+import { strict as assert } from 'assert';
+import { TextDecoder } from 'util';
+
+import {
+  BlockMetaInformation,
+  IOType,
+  PropertyValuetype,
+  isTextLiteral,
+} from '@jvalue/language-server';
 
 export class TextFileInterpreterMetaInformation extends BlockMetaInformation {
   constructor() {
     super(
       'TextFileInterpreter',
-      {},
+      {
+        encoding: {
+          type: PropertyValuetype.TEXT,
+          defaultValue: 'utf-8',
+          docs: {
+            description: 'The encoding used for decoding the file contents.',
+          },
+          validation: (property, accept) => {
+            const propertyValue = property.value;
+            assert(isTextLiteral(propertyValue));
+
+            try {
+              new TextDecoder(propertyValue.value);
+            } catch (error) {
+              accept('error', `Unknown encoding "${propertyValue.value}"`, {
+                node: propertyValue,
+              });
+            }
+          },
+        },
+        lineBreak: {
+          type: PropertyValuetype.REGEX,
+          defaultValue: '\r?\n',
+          docs: {
+            description: 'The regex for identifying line breaks.',
+          },
+        },
+      },
       // Input type:
       IOType.FILE,
 
