908f8c200d3beda6fdf389e9a53e7455dc88b91b
schlingling <schilling.jn@icloud.com>
2023-04-02 15:37:11 +0200

    added parser for tripupdates

--- libs/extensions/std/exec/src/gtfs-rt-interpreter-executor.ts
+++ libs/extensions/std/exec/src/gtfs-rt-interpreter-executor.ts
@@ -12,6 +12,8 @@ import {
   implementsStatic,
 } from '@jvalue/execution';
 import { IOType } from '@jvalue/language-server';
+import * as E from 'fp-ts/lib/Either';
+import { Either, isLeft } from 'fp-ts/lib/Either';
 import * as GtfsRealtimeBindings from 'gtfs-realtime-bindings';
 
 @implementsStatic<BlockExecutorClass>()
@@ -35,28 +37,26 @@ export class GtfsRTInterpreterExecutor
         new Uint8Array(inputFile.content),
       );
 
-    // TODO: Parse all possible feedentity to table
-    const parsedFeedMessage = await this.parseFeedMessage(
-      entity,
-      feedMessage,
-      context,
-    );
-    if (R.isErr(parsedFeedMessage)) {
-      return parsedFeedMessage;
+    // Parse all possible feedentity to Sheet
+    const sheet = await this.parseFeedMessage(entity, feedMessage, context);
+    if (R.isErr(sheet)) {
+      return sheet;
     }
 
-    return R.ok(parsedFeedMessage.right);
+    return R.ok(sheet.right);
   }
 
   private parseFeedMessage(
-    entity: string,
+    entity_type: string,
     feedMessage: GtfsRealtimeBindings.transit_realtime.FeedMessage,
     context: ExecutionContext,
   ): Promise<R.Result<Sheet>> {
     return new Promise((resolve) => {
-      switch (entity) {
+      let data: Promise<E.Either<Error, string[][]>>;
+      switch (entity_type) {
         case 'trip_update':
-          // TODO;
+          // Extract the trip updates from thee feed message
+          data = this.parseTripUpdates(feedMessage);
           break;
         case 'alert':
           // TODO;
@@ -75,6 +75,71 @@ export class GtfsRTInterpreterExecutor
           );
           break;
       }
+      resolve(R.ok(new Sheet()));
     });
   }
+
+  private parseTripUpdates(
+    feedMessage: GtfsRealtimeBindings.transit_realtime.FeedMessage,
+  ): Promise<Either<Error, string[][]>> {
+    return new Promise((resolve) => {
+      const header: string[] = Object.keys({} as TripUpdates);
+      const rows: string[][] = [];
+      rows.push(header);
+      for (const entity of feedMessage.entity) {
+        if (entity.tripUpdate) {
+          if (entity.tripUpdate.stopTimeUpdate) {
+            for (const stop_time_update of entity.tripUpdate.stopTimeUpdate) {
+              const tripUpdate: TripUpdates = {
+                'header.gtfs_realtime_version':
+                  feedMessage.header.gtfsRealtimeVersion,
+                'header.timestamp': String(feedMessage.header.timestamp),
+                'header.incrementality': String(
+                  feedMessage.header.incrementality,
+                ),
+                'entity.id': String(entity.id),
+                'entity.trip_update.trip.trip_id': String(
+                  entity.tripUpdate.trip.tripId,
+                ),
+                'entity.trip_update.trip.route_id': String(
+                  entity.tripUpdate.trip.routeId,
+                ),
+                'entity.trip_update.stop_time_update.stop_sequence': String(
+                  stop_time_update.stopSequence,
+                ),
+                'entity.trip_update.stop_time_update.stop_id': String(
+                  stop_time_update.stopId,
+                ),
+                'entity.trip_update.stop_time_update.arrival.time': String(
+                  stop_time_update.arrival?.time,
+                ),
+                'entity.trip_update.stop_time_update.departure.time': String(
+                  stop_time_update.departure?.time,
+                ),
+              };
+              rows.push(Object.values(tripUpdate) as string[]);
+            }
+          } else {
+            // TODO: Error case with left
+          }
+        } else {
+          // TODO: Error case with left
+        }
+      }
+      resolve(E.right(rows));
+    });
+  }
+}
+
+interface TripUpdates {
+  'header.gtfs_realtime_version': string;
+  'header.timestamp': string;
+  'header.incrementality': string;
+  'entity.id': string;
+  'entity.trip_update.trip.trip_id': string;
+  'entity.trip_update.trip.route_id': string;
+  'entity.trip_update.stop_time_update.stop_sequence': string;
+  'entity.trip_update.stop_time_update.stop_id': string;
+  'entity.trip_update.stop_time_update.arrival.time': string;
+  'entity.trip_update.stop_time_update.departure.time': string;
 }
