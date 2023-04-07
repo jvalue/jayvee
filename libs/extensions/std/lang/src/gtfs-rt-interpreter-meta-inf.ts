0ec58309eb990ab403c8478b6379b080f0b3edaf
schlingling <schilling.jn@icloud.com>
2023-04-07 16:59:02 +0200

    moved docs from executor to meta-inf

--- libs/extensions/std/lang/src/gtfs-rt-interpreter-meta-inf.ts
+++ libs/extensions/std/lang/src/gtfs-rt-interpreter-meta-inf.ts
@@ -19,21 +19,54 @@ export class GtfsRTInterpreterMetaInformation extends BlockMetaInformation {
         entity: {
           type: PropertyValuetype.TEXT,
           docs: {
-            description: `Entity to process from GTFS-RT-feed (\`vehiclePosition\`, \`tripUpdate\` or \`alert\`). For example, parsing entity \`tripUpdate\` outputs an \`Sheet\` with header:
+            description: `Entity to process from GTFS-RT-feed (\`trip_update\`, \`alert\` or \`vehicle\`).
+            We currently support following Output-Sheets:
+
+            Entity tripUpdate:
             \`\`\`
             [
-              "header.gtfs_realtime_version",
-              "header.timestamp",
-              "header.incrementality",
-              "entity.id",
-              "entity.trip_update.trip.trip_id",
-              "entity.trip_update.trip.route_id",
-              "entity.trip_update.stop_time_update.stop_sequence",
-              "entity.trip_update.stop_time_update.stop_id",
-              "entity.trip_update.stop_time_update.arrival.time",
-              "entity.trip_update.stop_time_update.departure.time"
+              "header.gtfs_realtime_version", // Version of speed specification. Currently "2.0"
+              "header.timestamp", // The moment where this dataset was generated on the server.
+              "header.incrementality", // DIFFERENTIAL is unsupported. use FULL_DATASET only
+              "entity.id", // Unique identifier for the entity within the feed update message
+              "entity.trip_update.trip.trip_id", // The trip_id from the GTFS feed that this selector refers to. Whether trip_id is required depends on the type of trip
+              "entity.trip_update.trip.route_id", // The route_id from the GTFS feed that this selector refers to. I
+              "entity.trip_update.stop_time_update.stop_sequence", // Must be the same as in stop_times.txt in the corresponding GTFS feed. Either stop_sequence or stop_id must be provided within a StopTimeUpdate - both fields cannot be empty. stop_sequence is required for trips that visit the same stop_id more than once (e.g., a loop) to disambiguate which stop the prediction is for.
+              "entity.trip_update.stop_time_update.stop_id", // Must be the same as in stops.txt in the corresponding GTFS feed. Either stop_sequence or stop_id must be provided within a StopTimeUpdate - both fields cannot be empty.
+              "entity.trip_update.stop_time_update.arrival.time", // If schedule_relationship is empty or SCHEDULED, either arrival or departure must be provided within a StopTimeUpdate - both fields cannot be empty. arrival and departure may both be empty when schedule_relationship is SKIPPED. If schedule_relationship is NO_DATA, arrival and departure must be empty.
+              "entity.trip_update.stop_time_update.departure.time" // If schedule_relationship is empty or SCHEDULED, either arrival or departure must be provided within a StopTimeUpdate - both fields cannot be empty. arrival and departure may both be empty when schedule_relationship is SKIPPED. If schedule_relationship is NO_DATA, arrival and departure must be empty.
             ]
             \`\`\`
+
+            Entity vehiclePosition:
+            \`\`\`
+            [
+              "header.gtfs_realtime_version", // Version of speed specification. Currently "2.0"
+              "header.timestamp", // The moment where this dataset was generated on the server.
+              "header.incrementality", // DIFFERENTIAL is unsupported. use FULL_DATASET only
+              "entity.id", // Unique identifier for the entity within the feed update message
+              "entity.vehicle_position.vehicle_descriptor.id", // Internal system identification of the vehicle. Should be unique per vehicle, and is used for tracking the vehicle as it proceeds through the system. This id should not be made visible to the end-user; for that purpose use the label field.
+              "entity.vehicle_position.trip.trip_id", // The trip_id from the GTFS feed that this selector refers to. Whether trip_id is required depends on the type of trip
+              "entity.vehicle_position.trip.route_id", // The route_id from the GTFS feed that this selector refers to.
+              "entity.vehicle_position.position.latitude", // Degrees North, in the WGS-84 coordinate system.
+              "entity.vehicle_position.position.longitude", // Degrees East, in the WGS-84 coordinate system.
+              "entity.vehicle_position.timestamp" // Moment at which the vehicle's position was measured. In POSIX time (i.e., number of seconds since January 1st 1970 00:00:00 UTC).
+              ]
+            \`\`\`
+
+            Entity alert:
+            \`\`\`
+            [
+              "header.gtfs_realtime_version", // Version of speed specification. Currently "2.0"
+              "header.timestamp", // The moment where this dataset was generated on the server.
+              "header.incrementality", // DIFFERENTIAL is unsupported. use FULL_DATASET only
+              "entity.id", // Unique identifier for the entity within the feed update message
+              "entity.alert.informed_entity.route_id", // The route_id from the GTFS that this selector refers to. If direction_id is provided, route_id must also be provided.
+              "entity.alert.header_text", // Header for the alert. This plain-text string will be highlighted, for example in boldface.
+              "entity.alert.description_text" // Description for the alert. This plain-text string will be formatted as the body of the alert (or shown on an explicit "expand" request by the user). The information in the description should add to the information of the header.
+              ]
+            \`\`\`
+
             `,
           },
         },
