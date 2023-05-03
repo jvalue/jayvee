e5d2a8e3291919c9218b84864358024301aaba9b
Georg Schwarz <dev@georg-schwarz.com>
2023-05-03 10:41:58 +0200

    Add check for transformer ports

--- libs/language-server/src/lib/validation/checks/transformer-body.ts
+++ libs/language-server/src/lib/validation/checks/transformer-body.ts
@@ -11,4 +11,39 @@ export function validateTransformerBody(
   context: ValidationContext,
 ): void {
   checkUniqueNames(transformerBody.ports, context, 'transformer port');
+  checkUniqueOutputAssignments(transformerBody, context);
+}
+
+export function checkUniqueOutputAssignments(
+  transformerBody: TransformerBody,
+  context: ValidationContext,
+): void {
+  const assignedOutputPorts = transformerBody.outputAssignments;
+  const definedOutputPorts = transformerBody.ports.filter(
+    (x) => x.kind === 'to',
+  );
+
+  for (const definedOutputPort of definedOutputPorts) {
+    const usedInAssignments = assignedOutputPorts.filter(
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      (x) => x.outPortName?.ref?.name === definedOutputPort.name,
+    );
+
+    if (usedInAssignments.length === 0) {
+      context.accept(
+        'error',
+        'An output assignment is required for this port',
+        { node: definedOutputPort, property: 'name' },
+      );
+    }
+
+    if (usedInAssignments.length > 1) {
+      usedInAssignments.forEach((usedAssignment) => {
+        context.accept('error', 'At most one assignment per output port', {
+          node: usedAssignment,
+          property: 'outPortName',
+        });
+      });
+    }
+  }
 }
