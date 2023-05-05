fdf37198ac633c02b7a49a1fe22743603bf624df
Georg Schwarz <dev@georg-schwarz.com>
2023-05-05 21:59:52 +0200

    Add missing license header

--- libs/language-server/src/lib/ast/wrappers/value-type/primitive/index.ts
+++ libs/language-server/src/lib/ast/wrappers/value-type/primitive/index.ts
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
+// eslint-disable-next-line import/no-cycle
 export * from './boolean-valuetype';
 export * from './decimal-valuetype';
 export * from './integer-valuetype';
