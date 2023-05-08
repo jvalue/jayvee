c6a08038f19426453a5ad90e66229b861ac2095c
Georg Schwarz <dev@georg-schwarz.com>
2023-05-08 10:26:16 +0200

    Extract constants to own file

---
+++ libs/execution/src/lib/types/valuetypes/constants.ts
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+export const DECIMAL_COMMA_SEPARATOR_REGEX = /^[+-]?([0-9]*[,])?[0-9]+$/;
+export const DECIMAL_DOT_SEPARATOR_REGEX = /^[+-]?([0-9]*[.])?[0-9]+$/;
+
+export const BOOLEAN_STRING_REPRESENTATIONS = [
+  'true',
+  'True',
+  'false',
+  'False',
+];
