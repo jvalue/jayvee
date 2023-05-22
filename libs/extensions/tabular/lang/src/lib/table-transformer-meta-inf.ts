054e490d9b83d60e3ba2327615f9a3576fd5b13e
Georg Schwarz <dev@georg-schwarz.com>
2023-05-22 11:44:04 +0200

    Add docs to TableTransformerMetaInformation

--- libs/extensions/tabular/lang/src/lib/table-transformer-meta-inf.ts
+++ libs/extensions/tabular/lang/src/lib/table-transformer-meta-inf.ts
@@ -15,16 +15,56 @@ export class TableTransformerMetaInformation extends BlockMetaInformation {
       {
         inputColumn: {
           type: PrimitiveValuetypes.Text,
+          docs: {
+            description:
+              "The name of the input column. Has to be present in the table and match with the transform's input port type.",
+          },
         },
         outputColumn: {
           type: PrimitiveValuetypes.Text,
+          docs: {
+            description:
+              'The name of the output column. Overwrites the column if it already exists, or otherwise creates a new one.',
+          },
         },
         use: {
           type: PrimitiveValuetypes.Transform,
+          docs: {
+            description:
+              'Reference to the transform that is applied to the column.',
+          },
         },
       },
       IOType.TABLE,
       IOType.TABLE,
     );
+    this.docs = {
+      description:
+        'Applies a transform on each value of a column. The input port type of the used transform has to match the type of the input column.',
+      examples: [
+        {
+          description:
+            'Given a column "temperature" with temperature values in Celsius, it overwrites the column with computed values in Fahrenheit by using the `CelsiusToFahrenheit` transform defined elsewhere.',
+          code: blockExampleOverwrite,
+        },
+        {
+          description:
+            'Given a column "temperatureCelsius" with temperature values in Celsius, it adds a new column "temperatureFahrenheit" with computed values in Fahrenheit by using the `CelsiusToFahrenheit` transform defined elsewhere.',
+          code: blockExampleNewCol,
+        },
+      ],
+    };
   }
 }
+
+const blockExampleOverwrite = `block CelsiusToFahrenheitTransformer oftype TableTransformer {
+  inputColumn: 'temperature';
+  outputColumn: 'temperature';
+  use: CelsiusToFahrenheit;
+}`;
+
+const blockExampleNewCol = `block CelsiusToFahrenheitTransformer oftype TableTransformer {
+  inputColumn: 'temperatureCelsius';
+  outputColumn: 'temperatureFahrenheit';
+  use: CelsiusToFahrenheit;
+}`;
