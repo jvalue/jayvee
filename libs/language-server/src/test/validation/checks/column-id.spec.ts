0d8933792f8d973195c0e2f2a9044271ed90509e
Felix Müller <felix.mueller0998@gmx.de>
2023-05-24 14:53:38 +0200

    Added missing licenses

--- libs/language-server/src/test/validation/checks/column-id.spec.ts
+++ libs/language-server/src/test/validation/checks/column-id.spec.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { StdLangExtension } from '@jvalue/jayvee-extensions/std/lang';
 import { AstNode, LangiumDocument } from 'langium';
