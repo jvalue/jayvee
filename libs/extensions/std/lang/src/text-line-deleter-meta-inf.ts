8c20b18d3f88d7c9e786484c4e89bd6ed7c97eb7
felix-oq <51856713+felix-oq@users.noreply.github.com>
2023-06-12 13:24:26 +0200

    Use verbose syntax for index

--- libs/extensions/std/lang/src/text-line-deleter-meta-inf.ts
+++ libs/extensions/std/lang/src/text-line-deleter-meta-inf.ts
@@ -31,7 +31,7 @@ export class TextLineDeleterMetaInformation extends BlockMetaInformation {
                   {
                     node: property.value,
                     property: 'values',
-                    index,
+                    index: index,
                   },
                 );
               }
