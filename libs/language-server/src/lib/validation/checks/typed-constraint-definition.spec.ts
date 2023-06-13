b3aad9587d313190498633cfa209223d110e0aa6
Felix Müller <felix.mueller0998@gmx.de>
2023-06-13 21:57:43 +0200

    Added typedConstraintDefinition validation tests

---
+++ libs/language-server/src/lib/validation/checks/typed-constraint-definition.spec.ts
@@ -0,0 +1,94 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { AstNode, AstNodeLocator, LangiumDocument } from 'langium';
+import { NodeFileSystem } from 'langium/node';
+
+import {
+  TypedConstraintDefinition,
+  ValidationContext,
+  createJayveeServices,
+  useExtension,
+} from '../../../lib';
+import {
+  ParseHelperOptions,
+  expectNoParserAndLexerErrors,
+  parseHelper,
+  readJvTestAssetHelper,
+  validationAcceptorMockImpl,
+} from '../../../test';
+import { TestLangExtension } from '../../../test/extension';
+
+import { validateTypedConstraintDefinition } from './typed-constraint-definition';
+
+describe('typed-constraint-definition validation tests', () => {
+  let parse: (
+    input: string,
+    options?: ParseHelperOptions,
+  ) => Promise<LangiumDocument<AstNode>>;
+
+  const validationAcceptorMock = jest.fn(validationAcceptorMockImpl);
+
+  let locator: AstNodeLocator;
+
+  const readJvTestAsset = readJvTestAssetHelper(
+    __dirname,
+    '../../../test/assets/',
+  );
+
+  async function parseAndValidateTypedConstraintDefinition(input: string) {
+    const document = await parse(input);
+    expectNoParserAndLexerErrors(document);
+
+    const typedConstraint = locator.getAstNode<TypedConstraintDefinition>(
+      document.parseResult.value,
+      'constraints@0',
+    ) as TypedConstraintDefinition;
+
+    validateTypedConstraintDefinition(
+      typedConstraint,
+      new ValidationContext(validationAcceptorMock),
+    );
+  }
+
+  beforeAll(() => {
+    // Register test extension
+    useExtension(new TestLangExtension());
+    // Create language services
+    const services = createJayveeServices(NodeFileSystem).Jayvee;
+    locator = services.workspace.AstNodeLocator;
+    // Parse function for Jayvee (without validation)
+    parse = parseHelper(services);
+  });
+
+  afterEach(() => {
+    // Reset mock
+    validationAcceptorMock.mockReset();
+  });
+
+  it('should have no error on valid typed constraint', async () => {
+    const text = readJvTestAsset(
+      'typed-constraint-definition/valid-typed-constraint.jv',
+    );
+
+    await parseAndValidateTypedConstraintDefinition(text);
+
+    expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+  });
+
+  it('error on unknown constraint type', async () => {
+    const text = readJvTestAsset(
+      'typed-constraint-definition/invalid-unknown-constraint-type.jv',
+    );
+
+    await parseAndValidateTypedConstraintDefinition(text);
+
+    expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+    expect(validationAcceptorMock).toHaveBeenCalledWith(
+      'error',
+      `Unknown constraint type 'UnknownConstraint'`,
+      expect.any(Object),
+    );
+  });
+});
