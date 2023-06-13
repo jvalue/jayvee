a962a0747b26e6c695609a5d9fd14739bcbd6fd2
Felix Müller <felix.mueller0998@gmx.de>
2023-06-13 21:59:06 +0200

    Added expressionConstraintDefinition validation tests

---
+++ libs/language-server/src/lib/validation/checks/expression-constraint-definition.spec.ts
@@ -0,0 +1,95 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { AstNode, AstNodeLocator, LangiumDocument } from 'langium';
+import { NodeFileSystem } from 'langium/node';
+
+import {
+  ExpressionConstraintDefinition,
+  ValidationContext,
+  createJayveeServices,
+  useExtension,
+} from '../../../lib';
+import {
+  ParseHelperOptions,
+  expectNoParserAndLexerErrors,
+  parseHelper,
+  readJvTestAssetHelper,
+  validationAcceptorMockImpl,
+} from '../../../test';
+import { TestLangExtension } from '../../../test/extension';
+
+import { validateExpressionConstraintDefinition } from './expression-constraint-definition';
+
+describe('expression-constraint-definition validation tests', () => {
+  let parse: (
+    input: string,
+    options?: ParseHelperOptions,
+  ) => Promise<LangiumDocument<AstNode>>;
+
+  const validationAcceptorMock = jest.fn(validationAcceptorMockImpl);
+
+  let locator: AstNodeLocator;
+
+  const readJvTestAsset = readJvTestAssetHelper(
+    __dirname,
+    '../../../test/assets/',
+  );
+
+  async function parseAndValidateExpressionConstraintDefinition(input: string) {
+    const document = await parse(input);
+    expectNoParserAndLexerErrors(document);
+
+    const expressionConstraint =
+      locator.getAstNode<ExpressionConstraintDefinition>(
+        document.parseResult.value,
+        'constraints@0',
+      ) as ExpressionConstraintDefinition;
+
+    validateExpressionConstraintDefinition(
+      expressionConstraint,
+      new ValidationContext(validationAcceptorMock),
+    );
+  }
+
+  beforeAll(() => {
+    // Register test extension
+    useExtension(new TestLangExtension());
+    // Create language services
+    const services = createJayveeServices(NodeFileSystem).Jayvee;
+    locator = services.workspace.AstNodeLocator;
+    // Parse function for Jayvee (without validation)
+    parse = parseHelper(services);
+  });
+
+  afterEach(() => {
+    // Reset mock
+    validationAcceptorMock.mockReset();
+  });
+
+  it('should have no error on valid expresion constraint', async () => {
+    const text = readJvTestAsset(
+      'expression-constraint-definition/valid-text-constraint.jv',
+    );
+
+    await parseAndValidateExpressionConstraintDefinition(text);
+
+    expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+  });
+
+  it('error on incompatible type', async () => {
+    const text = readJvTestAsset(
+      'expression-constraint-definition/invalid-incompatible-type.jv',
+    );
+
+    await parseAndValidateExpressionConstraintDefinition(text);
+
+    expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+    expect(validationAcceptorMock).toHaveBeenCalledWith(
+      'error',
+      `The value needs to be of type boolean but is of type integer`,
+      expect.any(Object),
+    );
+  });
+});
