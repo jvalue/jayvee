bff5f11914c8aee837f2eeb1c6480f6480a30d54
Felix Müller <felix.mueller0998@gmx.de>
2023-06-22 12:44:19 +0200

    Refactored testExtension such that it generates basic Extractors and Loaders during runtime

--- libs/language-server/src/test/extension/extension.ts
+++ libs/language-server/src/test/extension/extension.ts
@@ -5,21 +5,47 @@
 import {
   BlockMetaInformation,
   ConstructorClass,
+  IOType,
   JayveeLangExtension,
 } from '../../lib';
 
-import {
-  TestFileExtractorMetaInformation,
-  TestPropertyMetaInformation,
-  TestTableLoaderMetaInformation,
-} from './lib';
+import { TestPropertyMetaInformation } from './lib';
 
 export class TestLangExtension implements JayveeLangExtension {
   getBlockMetaInf(): Array<ConstructorClass<BlockMetaInformation>> {
+    const ioTypes = Object.values(IOType);
     return [
-      TestFileExtractorMetaInformation,
       TestPropertyMetaInformation,
-      TestTableLoaderMetaInformation,
+      ...ioTypes.map((ioType) =>
+        this.constructMetaInformationFromIOType(ioType, 'input'),
+      ),
+      ...ioTypes.map((ioType) =>
+        this.constructMetaInformationFromIOType(ioType, 'output'),
+      ),
     ];
+  }
+
+  /**
+   * Constructs BlockMetaInformation that is either an loader or extractor block, depending on the io param.
+   * The name of the new Block is constructed `Test${ioType}${io === 'input' ? 'Loader' : 'Extractor'}`.
+   * For example if ioType = IOType.File and io = 'input' -> 'TestFileLoader'
+   * @param ioType IOType of the block input or output (depending on io param)
+   * @param io specifies whether the given ioType param is the input or output of the block
+   * @returns ConstructorClass<BlockMetaInformation>
+   */
+  constructMetaInformationFromIOType(
+    ioType: IOType,
+    io: 'input' | 'output',
+  ): ConstructorClass<BlockMetaInformation> {
+    return class TestMetaInformation extends BlockMetaInformation {
+      constructor() {
+        super(
+          `Test${ioType}${io === 'input' ? 'Loader' : 'Extractor'}`,
+          {},
+          io === 'input' ? ioType : IOType.NONE,
+          io === 'input' ? IOType.NONE : ioType,
+        );
+      }
+    };
   }
 }
--- libs/language-server/src/test/extension/lib/index.ts
+++ libs/language-server/src/test/extension/lib/index.ts
@@ -2,6 +2,4 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-export * from './test-file-extractor-meta-inf';
-export * from './test-table-loader-meta-inf';
 export * from './test-property-meta-inf';
