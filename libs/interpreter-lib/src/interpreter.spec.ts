becd548837ca92040521e391c8e852cb2144f78d
Georg Schwarz <dev@georg-schwarz.com>
2023-07-05 19:48:59 +0200

    Add license headers to new file

--- libs/interpreter-lib/src/interpreter.spec.ts
+++ libs/interpreter-lib/src/interpreter.spec.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 import { readJvTestAssetHelper } from '@jvalue/jayvee-language-server/test';
 
 import { ExitCode } from './cli-util';
