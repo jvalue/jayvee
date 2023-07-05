f3db40c22ee78afc1281b502c78c5b68024e6746
Georg Schwarz <dev@georg-schwarz.com>
2023-07-05 12:46:55 +0200

    Add empty library interpreter-lib

---
+++ libs/interpreter-lib/jest.config.ts
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+export default {
+  displayName: 'jayvee-interpreter-lib',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/@jvalue/jayvee-interpreter-lib',
+};
