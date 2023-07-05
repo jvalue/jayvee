39123fd531ecba016f3762fa1ea373c6cf9486cb
Georg Schwarz <dev@georg-schwarz.com>
2023-07-05 19:50:39 +0200

    Rename cli-util to parsing-util

--- libs/interpreter-lib/src/index.ts
+++ libs/interpreter-lib/src/index.ts
@@ -3,6 +3,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 export * from './interpreter';
-export * from './cli-util';
+export * from './parsing-util';
 export * from './logging';
 export * from './validation-checks';
