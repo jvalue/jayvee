14b93ada8da467c0af7c393333202f1aa1bf991a
Georg Schwarz <dev@georg-schwarz.com>
2023-07-17 17:08:45 +0200

    Add example valuetype to standard library

--- libs/language-server/src/lib/builtin-library/jayvee-standard-library.ts
+++ libs/language-server/src/lib/builtin-library/jayvee-standard-library.ts
@@ -5,5 +5,8 @@
 export const STANDARD_LIBRARY_FILENAME = 'standard.jv';
 
 export const STANDARD_LIBRARY_SOURCECODE = `
-// TBD
-`.trimLeft();
+valuetype Percent oftype decimal {
+    constraints: [ZeroToHundredDecimal];
+}
+constraint ZeroToHundredDecimal on decimal: value >= 0 and value <= 100;
+`.trimStart();
