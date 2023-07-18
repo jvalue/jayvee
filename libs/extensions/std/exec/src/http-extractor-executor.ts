a23642af5402a8ba6c37491c04b45fb6c0ee9c41
A-M-A-X <maximilian.ackermann@icloud.com>
2023-07-18 16:54:39 +0200

    removed unused imports

--- libs/extensions/std/exec/src/http-extractor-executor.ts
+++ libs/extensions/std/exec/src/http-extractor-executor.ts
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-// import * as http from 'http';
-// import * as https from 'https';
 import * as path from 'path';
 import { http, https } from 'follow-redirects';
 
