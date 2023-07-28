e35b30695d9f5a3bc21869cb4e54a7035d454550
Georg Schwarz <dev@georg-schwarz.com>
2023-07-28 14:02:07 +0200

    Register std extension in vscode to load blocktypes into StdLib

--- apps/vs-code-extension/src/standard-library-file-system-provider.ts
+++ apps/vs-code-extension/src/standard-library-file-system-provider.ts
@@ -2,7 +2,11 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import { getStdLib } from '@jvalue/jayvee-language-server';
+import { StdLangExtension } from '@jvalue/jayvee-extensions/std/lang';
+import {
+  getStdLib,
+  useExtension as useLangExtension,
+} from '@jvalue/jayvee-language-server';
 import {
   EventEmitter,
   ExtensionContext,
@@ -23,11 +27,14 @@ export class StandardLibraryFileSystemProvider implements FileSystemProvider {
   onDidChangeFile = this.didChangeFile.event;
 
   constructor() {
-    // TODO: metainf registry is not initialized => cannot load documents
     this.registerStdLib();
   }
 
   private registerStdLib() {
+    // The VSCode Extension needs to register the StdLangExtension,
+    // otherwise the StdLib does not include the blocktype definitions.
+    useLangExtension(new StdLangExtension());
+
     Object.entries(getStdLib()).forEach(([libName, lib]) => {
       this.libraries.set(
         Uri.parse(libName).toString(), // removes slashes if missing authorities, required for matching later on
