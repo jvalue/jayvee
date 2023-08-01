22f70c88b233ecede476c719cf38842fb61cfd2c
Georg Schwarz <dev@georg-schwarz.com>
2023-08-01 11:20:06 +0200

    Enable collection valuetype creation for collections

--- libs/language-server/src/lib/ast/wrappers/value-type/primitive/collection/collection-valuetype.ts
+++ libs/language-server/src/lib/ast/wrappers/value-type/primitive/collection/collection-valuetype.ts
@@ -2,12 +2,16 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
+import { strict as assert } from 'assert';
+
 import {
   type AtomicInternalValueRepresentation,
   type InternalValueRepresentation,
 } from '../../../../expressions/internal-value-representation';
+import { ValuetypeReference } from '../../../../generated/ast';
 // eslint-disable-next-line import/no-cycle
 import { Valuetype, ValuetypeVisitor } from '../../valuetype';
+import { createValuetype } from '../../valuetype-util';
 
 import {
   AbstractCollectionValuetype,
@@ -52,4 +56,27 @@ export function isCollectionValuetype<
   I extends AtomicInternalValueRepresentation,
 >(v: unknown, elementType: Valuetype<I>): v is CollectionValuetype<I> {
   return v instanceof CollectionValuetype && v.elementType.equals(elementType);
+}
+
+export function createCollectionValuetype(
+  collectionRef: ValuetypeReference,
+): CollectionValuetype {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  const collectionDefinition = collectionRef?.reference?.ref;
+  assert(collectionDefinition?.name === 'Collection');
+  const collectionGenerics = collectionRef.genericRefs;
+  if (collectionGenerics.length !== 1) {
+    throw new Error(
+      "Valuetype Collection needs exactly one generic parameter to define its elements' type",
+    );
+  }
+  const generic = collectionGenerics[0];
+  assert(generic !== undefined);
+  const elementValuetype = createValuetype(generic.ref);
+  if (elementValuetype === undefined) {
+    throw new Error(
+      "Could not create valuetype for the elements' type of valuetype Collection",
+    );
+  }
+  return new CollectionValuetype(elementValuetype);
 }
