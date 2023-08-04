7fcb277276e2128f04a7e761c53779cf56a326b0
Georg Schwarz <dev@georg-schwarz.com>
2023-08-04 15:18:21 +0200

    Add script to automatically generate new docs snapshot version

--- apps/docs/docs/dev/02-dev-processes/04-release-jayvee-version.md
+++ apps/docs/docs/dev/02-dev-processes/04-release-jayvee-version.md
@@ -15,9 +15,7 @@ To indicate that, we only release alpha versions where the `version` is incremen
 For releasing a new version of Jayvee, you need to complete the following steps:
 
 1. Increment the version in the `package.json` file.
-2. Generate the docs by `npx nx run docs:generate`.
-3. Create a docs version with `npx docusaurus docs:version <version-number>`.
-4. Delete all `.gitignore` files in the created versioned docs under `apps/docs/versioned_docs/version-0.0.<version>` to check-in the generated files.
-5. If you are on a feature or dev branch, merge into main.
-6. Create a GitHub release on the main branch. Attach a changelog.
-7. The CI/CD will deal with the rest.
+2. Run `npx nx run docs:version-snapshot`.
+3. If you are on a feature or dev branch, merge into main.
+4. Create a GitHub release on the main branch. Attach a changelog.
+5. The CI/CD will deal with the rest.
---
+++ tools/scripts/docs/create-new-version-snapshot.mjs
@@ -0,0 +1,69 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import {
+    parsePackageJson, getSourcePath
+} from '../shared-util.mjs';
+import { workspaceRoot } from "@nrwl/devkit";
+
+import fs from "fs";
+import path from 'path';
+import { execSync } from 'child_process';
+
+// Executing this script: node path/to/prepare-new-version-snapshot.mjs
+
+const licenseHeader = "SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg"
+    + "\n\n"
+    + "SPDX-License-Identifier: AGPL-3.0-only";
+
+process.chdir(workspaceRoot);
+const packageJson = parsePackageJson();
+const versionTag = packageJson.version
+
+const pathToDocsVersion = path.join(getSourcePath('docs'), '..', 'versioned_docs', `version-${versionTag}`);
+const pathToSidebarsVersion = path.join(getSourcePath('docs'), '..', 'versioned_sidebars');
+
+createDocusaurusSnapshot();
+deleteGitignoreRecursively(pathToDocsVersion);
+addLicenseHeaderToFilesRecursive(pathToDocsVersion)
+addLicenseHeaderToFilesRecursive(pathToSidebarsVersion)
+
+function createDocusaurusSnapshot() {
+  const pathToDocsProject = getSourcePath('docs');
+  process.chdir(path.join(pathToDocsProject, '..'))
+  execSync(`npx docusaurus docs:version ${versionTag}`);
+  console.log(`Created docs snapshot for version ${versionTag}`);
+  process.chdir(workspaceRoot);
+}
+
+function deleteGitignoreRecursively(dir) {
+    for (const file of fs.readdirSync(dir)) {
+        const filePath = path.join(dir, file);
+      const fileStats = fs.statSync(filePath);
+
+      if (fileStats.isDirectory()) {
+        deleteGitignoreRecursively(filePath);
+      } else if (file === '.gitignore') {
+        fs.unlinkSync(filePath);
+        console.log(`Deleted .gitignore file at ${filePath}`);
+      }
+    }
+}
+
+function addLicenseHeaderToFilesRecursive(dir) {
+    for (const file of fs.readdirSync(dir)) {
+        const filePath = path.join(dir, file);
+      const fileStats = fs.statSync(filePath);
+
+      if (fileStats.isDirectory()) {
+        addLicenseHeaderToFilesRecursive(filePath);
+      } else if (!file.endsWith('.license')){
+        const licenseFilePath = path.join(dir, `${file}.license`)
+        if (!fs.existsSync(licenseFilePath)) {
+            fs.writeFileSync(licenseFilePath, licenseHeader)
+            console.log(`Created license file at ${filePath}`);
+        }
+      }
+    }
+}
