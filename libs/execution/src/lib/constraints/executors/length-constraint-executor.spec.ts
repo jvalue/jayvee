5a09036850b39fd7c040a33728654093f618eb1a
Felix Müller <felix.mueller0998@gmx.de>
2023-08-21 13:06:41 +0200

    Basic tests for ExpressionConstraintExecutor, LengthConstraintExecutor and RegexConstraintExecutor

---
+++ libs/execution/src/lib/constraints/executors/length-constraint-executor.spec.ts
@@ -0,0 +1,91 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import {
+  BlockDefinition,
+  InternalValueRepresentation,
+  TypedConstraintDefinition,
+  createJayveeServices,
+  useExtension,
+} from '@jvalue/jayvee-language-server';
+import {
+  ParseHelperOptions,
+  TestLangExtension,
+  expectNoParserAndLexerErrors,
+  parseHelper,
+  readJvTestAssetHelper,
+} from '@jvalue/jayvee-language-server/test';
+import { AstNode, AstNodeLocator, LangiumDocument } from 'langium';
+import { NodeFileSystem } from 'langium/node';
+
+import { getTestExecutionContext } from '../../../../test/utils';
+
+import { LengthConstraintExecutor } from './length-constraint-executor';
+
+describe('Validation of LengthConstraintExecutor', () => {
+  let parse: (
+    input: string,
+    options?: ParseHelperOptions,
+  ) => Promise<LangiumDocument<AstNode>>;
+
+  let locator: AstNodeLocator;
+
+  const readJvTestAsset = readJvTestAssetHelper(
+    __dirname,
+    '../../../../test/assets/',
+  );
+
+  async function parseAndValidateConstraint(
+    input: string,
+    value: InternalValueRepresentation,
+  ): Promise<boolean> {
+    const document = await parse(input, { validationChecks: 'all' });
+    expectNoParserAndLexerErrors(document);
+
+    const usageBlock = locator.getAstNode<BlockDefinition>(
+      document.parseResult.value,
+      'pipelines@0/blocks@2',
+    ) as BlockDefinition;
+    const constraint = locator.getAstNode<TypedConstraintDefinition>(
+      document.parseResult.value,
+      'constraints@0',
+    ) as TypedConstraintDefinition;
+
+    return new LengthConstraintExecutor().isValid(
+      value,
+      // Execution context with initial stack containing usage block of constraint and constraint itself
+      getTestExecutionContext(locator, document, [usageBlock, constraint]),
+    );
+  }
+
+  beforeAll(() => {
+    // Register test extension
+    useExtension(new TestLangExtension());
+    // Create language services
+    const services = createJayveeServices(NodeFileSystem).Jayvee;
+    locator = services.workspace.AstNodeLocator;
+    // Parse function for Jayvee (without validation)
+    parse = parseHelper(services);
+  });
+
+  it('should diagnose no error on valid value', async () => {
+    const text = readJvTestAsset(
+      'length-constraint-executor/valid-constraint.jv',
+    );
+
+    const valid = await parseAndValidateConstraint(text, 'tt');
+
+    expect(valid).toBe(true);
+  });
+
+  it('should diagnose error on invalid value', async () => {
+    const text = readJvTestAsset(
+      'length-constraint-executor/valid-constraint.jv',
+    );
+
+    const valid = await parseAndValidateConstraint(text, 'ttttt');
+
+    expect(valid).toBe(false);
+  });
+});
