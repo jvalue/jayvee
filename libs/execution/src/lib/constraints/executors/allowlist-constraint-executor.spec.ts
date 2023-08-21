9f5faf10c07fa550ebade8f44b5391eebf20c252
Felix Müller <felix.mueller0998@gmx.de>
2023-08-21 10:24:46 +0200

    Added basic tests for AllowlistConstraintExecutor

---
+++ libs/execution/src/lib/constraints/executors/allowlist-constraint-executor.spec.ts
@@ -0,0 +1,93 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import {
+  BlockDefinition,
+  TypedConstraintDefinition,
+  createJayveeServices,
+  useExtension,
+} from '@jvalue/jayvee-language-server';
+import {
+  ParseHelperOptions,
+  TestLangExtension,
+  expectNoParserAndLexerErrors,
+  parseHelper,
+  readJvTestAssetHelper,
+} from '@jvalue/jayvee-language-server/test';
+import { AstNode, AstNodeLocator, LangiumDocument } from 'langium';
+import { NodeFileSystem } from 'langium/node';
+
+import { getTestExecutionContext } from '../../../../test/utils';
+
+import { AllowlistConstraintExecutor } from './allowlist-constraint-executor';
+
+describe('Validation of AllowlistConstraintExecutor', () => {
+  let parse: (
+    input: string,
+    options?: ParseHelperOptions,
+  ) => Promise<LangiumDocument<AstNode>>;
+
+  let locator: AstNodeLocator;
+
+  const readJvTestAsset = readJvTestAssetHelper(
+    __dirname,
+    '../../../../test/assets/',
+  );
+
+  async function parseAndValidateConstraint(
+    input: string,
+    value: string,
+  ): Promise<boolean> {
+    const document = await parse(input);
+    expectNoParserAndLexerErrors(document);
+
+    const allowlistUsageBlock = locator.getAstNode<BlockDefinition>(
+      document.parseResult.value,
+      'pipelines@0/blocks@2',
+    ) as BlockDefinition;
+    const allowlistConstraint = locator.getAstNode<TypedConstraintDefinition>(
+      document.parseResult.value,
+      'constraints@0',
+    ) as TypedConstraintDefinition;
+
+    return new AllowlistConstraintExecutor().isValid(
+      value,
+      // Execution context with initial stack containing usage block of constraint and constraint itself
+      getTestExecutionContext(locator, document, [
+        allowlistUsageBlock,
+        allowlistConstraint,
+      ]),
+    );
+  }
+
+  beforeAll(() => {
+    // Register test extension
+    useExtension(new TestLangExtension());
+    // Create language services
+    const services = createJayveeServices(NodeFileSystem).Jayvee;
+    locator = services.workspace.AstNodeLocator;
+    // Parse function for Jayvee (without validation)
+    parse = parseHelper(services);
+  });
+
+  it('should diagnose no error on valid value', async () => {
+    const text = readJvTestAsset(
+      'allowlist-constraint-executor/valid-constraint.jv',
+    );
+
+    const valid = await parseAndValidateConstraint(text, 'ms');
+
+    expect(valid).toBe(true);
+  });
+
+  it('should diagnose error on invalid value', async () => {
+    const text = readJvTestAsset(
+      'allowlist-constraint-executor/valid-constraint.jv',
+    );
+
+    const valid = await parseAndValidateConstraint(text, 'ly');
+
+    expect(valid).toBe(false);
+  });
+});
