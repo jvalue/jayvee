8cb933c11266d5e6fd10c4fa8ba10dc18988cae5
Georg Schwarz <dev@georg-schwarz.com>
2023-09-06 11:39:38 +0200

    Refactor blocktype specific property validations into own file

---
+++ libs/language-server/src/lib/validation/checks/blocktype-specific/properties.spec.ts
@@ -0,0 +1,433 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { AstNode, AstNodeLocator, LangiumDocument } from 'langium';
+import { NodeFileSystem } from 'langium/node';
+
+import {
+  BlockMetaInformation,
+  EvaluationContext,
+  MetaInformation,
+  PropertyBody,
+  PropertySpecification,
+  RuntimeParameterProvider,
+  ValidationContext,
+  createJayveeServices,
+  getConstraintMetaInf,
+  isBuiltinConstrainttypeDefinition,
+  isReferenceableBlocktypeDefinition,
+} from '../../../../lib';
+import {
+  ParseHelperOptions,
+  expectNoParserAndLexerErrors,
+  parseHelper,
+  readJvTestAssetHelper,
+  validationAcceptorMockImpl,
+} from '../../../../test';
+
+import { checkBlocktypeSpecificProperties } from './properties';
+
+describe('Validation of blocktype specific properties', () => {
+  let parse: (
+    input: string,
+    options?: ParseHelperOptions,
+  ) => Promise<LangiumDocument<AstNode>>;
+
+  const validationAcceptorMock = jest.fn(validationAcceptorMockImpl);
+
+  let locator: AstNodeLocator;
+
+  const readJvTestAsset = readJvTestAssetHelper(
+    __dirname,
+    '../../../../test/assets/',
+  );
+
+  async function parseAndValidatePropertyAssignment(input: string) {
+    const document = await parse(input);
+    expectNoParserAndLexerErrors(document);
+
+    const propertyBody = locator.getAstNode<PropertyBody>(
+      document.parseResult.value,
+      'pipelines@0/blocks@0/body',
+    ) as PropertyBody;
+
+    const type = propertyBody.$container.type;
+    let metaInf: MetaInformation | undefined;
+    if (isReferenceableBlocktypeDefinition(type.ref)) {
+      metaInf = new BlockMetaInformation(type.ref);
+    } else if (isBuiltinConstrainttypeDefinition(type.ref)) {
+      metaInf = getConstraintMetaInf(type.ref);
+    }
+    expect(metaInf).toBeDefined();
+
+    propertyBody.properties.forEach((propertyAssignment) => {
+      const propertySpec = (
+        metaInf as MetaInformation
+      ).getPropertySpecification(propertyAssignment.name);
+      expect(propertySpec).toBeDefined();
+
+      checkBlocktypeSpecificProperties(
+        propertyAssignment,
+        propertySpec as PropertySpecification,
+        new ValidationContext(validationAcceptorMock),
+        new EvaluationContext(new RuntimeParameterProvider()),
+      );
+    });
+  }
+
+  beforeAll(() => {
+    // Create language services
+    const services = createJayveeServices(NodeFileSystem).Jayvee;
+    locator = services.workspace.AstNodeLocator;
+    // Parse function for Jayvee (without validation)
+    parse = parseHelper(services);
+  });
+
+  afterEach(() => {
+    // Reset mock
+    validationAcceptorMock.mockReset();
+  });
+
+  describe('ArchiveInterpreter blocktype', () => {
+    it('should diagnose no error on valid archiveType parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/archive-interpreter/valid-valid-archivetype-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+
+    it('should diagnose error on invalid archiveType parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/archive-interpreter/invalid-invalid-archivetype-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'The value of property "archiveType" must be one of the following values: "zip", "gz"',
+        expect.any(Object),
+      );
+    });
+  });
+
+  describe('CellWriter blocktype', () => {
+    it('should diagnose error on wrong dimension for at parameter', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/cell-writer/invalid-wrong-at-dimension.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'The cell range needs to be one-dimensional',
+        expect.any(Object),
+      );
+    });
+  });
+
+  describe('ColumnDeleter blocktype', () => {
+    it('should diagnose error on deleting partial column', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/column-deleter/invalid-partial-column-delete.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'An entire column needs to be selected',
+        expect.any(Object),
+      );
+    });
+
+    it('should diagnose no error', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/column-deleter/valid-column-delete.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe('GtfsRTInterpreter blocktype', () => {
+    it('should diagnose no error on valid entity parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/gtfs-rt-interpreter/valid-valid-entity-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+
+    it('should diagnose error on invalid entity parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/gtfs-rt-interpreter/invalid-invalid-entity-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'The value of property "entity" must be one of the following values: "trip_update", "alert", "vehicle"',
+        expect.any(Object),
+      );
+    });
+  });
+
+  describe('HttpExtractor blocktype', () => {
+    it('should diagnose no error on valid retries parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/http-extractor/valid-valid-retries-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+
+    it('should diagnose error on invalid retries parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/http-extractor/invalid-invalid-retries-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'The value of property "retries" must not be smaller than 0',
+        expect.any(Object),
+      );
+    });
+
+    it('should diagnose no error on valid retryBackoffMilliseconds parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/http-extractor/valid-valid-retryBackoffMilliseconds-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+
+    it('should diagnose error on invalid retryBackoffMilliseconds parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/http-extractor/invalid-invalid-retryBackoffMilliseconds-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'The value of property "retryBackoffMilliseconds" must not be smaller than 1000',
+        expect.any(Object),
+      );
+    });
+
+    it('should diagnose no error on valid retryBackoffStrategy parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/http-extractor/valid-valid-retryBackoffStrategy-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+
+    it('should diagnose error on invalid retryBackoffStrategy parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/http-extractor/invalid-invalid-retryBackoffStrategy-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'The value of property "retryBackoffStrategy" must be one of the following values: "exponential", "linear"',
+        expect.any(Object),
+      );
+    });
+  });
+
+  describe('RowDeleter blocktype', () => {
+    it('should diagnose error on deleting partial row', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/row-deleter/invalid-partial-row-delete.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'An entire row needs to be selected',
+        expect.any(Object),
+      );
+    });
+
+    it('should diagnose no error', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/row-deleter/valid-row-delete.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe('TableInterpreter blocktype', () => {
+    it('should diagnose error on non unique column names', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/table-interpreter/invalid-non-unique-column-names.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(2);
+      expect(validationAcceptorMock).toHaveBeenNthCalledWith(
+        1,
+        'error',
+        'The column name "name" needs to be unique.',
+        expect.any(Object),
+      );
+      expect(validationAcceptorMock).toHaveBeenNthCalledWith(
+        2,
+        'error',
+        'The column name "name" needs to be unique.',
+        expect.any(Object),
+      );
+    });
+
+    it('should diagnose no error', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/table-interpreter/valid-correct-table.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe('TextFileInterpreter blocktype', () => {
+    it('should diagnose no error on valid encoding parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/text-file-interpreter/valid-utf8-encoding-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+
+    it('should diagnose error on invalid encoding parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/text-file-interpreter/invalid-invalid-encoding-param.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'The value of property "encoding" must be one of the following values: "utf8", "ibm866", "latin2", "latin3", "latin4", "cyrillic", "arabic", "greek", "hebrew", "logical", "latin6", "utf-16"',
+        expect.any(Object),
+      );
+    });
+  });
+
+  describe('TextLineDeleter blocktype', () => {
+    it('should diagnose no error on valid lines parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/text-line-deleter/valid-postive-line-number.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+
+    it('should diagnose error on invalid lines parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/text-line-deleter/invalid-line-less-or-equal-zero.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(3);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'Line numbers need to be greater than zero',
+        expect.any(Object),
+      );
+    });
+  });
+
+  describe('TextRangeSelector blocktype', () => {
+    it('should diagnose no error on valid lineFrom parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/text-range-selector/valid-postive-lineFrom-number.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+
+    it('should diagnose error on invalid lineFrom parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/text-range-selector/invalid-lineFrom-less-or-equal-zero.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'The value of property "lineFrom" must not be smaller than 1',
+        expect.any(Object),
+      );
+    });
+
+    it('should diagnose no error on valid lineTo parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/text-range-selector/valid-postive-lineTo-number.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(0);
+    });
+
+    it('should diagnose error on invalid lineTo parameter value', async () => {
+      const text = readJvTestAsset(
+        'property-assignment/blocktype-specific/text-range-selector/invalid-lineTo-less-or-equal-zero.jv',
+      );
+
+      await parseAndValidatePropertyAssignment(text);
+
+      expect(validationAcceptorMock).toHaveBeenCalledTimes(1);
+      expect(validationAcceptorMock).toHaveBeenCalledWith(
+        'error',
+        'The value of property "lineTo" must not be smaller than 1',
+        expect.any(Object),
+      );
+    });
+  });
+});
---
+++ libs/language-server/src/lib/validation/checks/blocktype-specific/properties.ts
@@ -0,0 +1,395 @@
+import {
+  CollectionValuetype,
+  EvaluationContext,
+  InternalValueRepresentation,
+  PrimitiveValuetypes,
+  PropertyAssignment,
+  evaluatePropertyValue,
+  isColumnWrapper,
+  isRowWrapper,
+} from '../../../ast';
+import { PropertySpecification } from '../../../meta-information';
+import { ValidationContext } from '../../validation-context';
+import { checkUniqueNames } from '../../validation-util';
+
+export function checkBlocktypeSpecificProperties(
+  property: PropertyAssignment,
+  propertySpec: PropertySpecification,
+  validationContext: ValidationContext,
+  evaluationContext: EvaluationContext,
+) {
+  const propName = property.name;
+  const propValue = evaluatePropertyValue(
+    property,
+    evaluationContext,
+    propertySpec.type,
+  );
+  if (propValue === undefined) {
+    return;
+  }
+
+  switch (property.$container.$container.type.ref?.name) {
+    case 'ArchiveInterpreter':
+      return checkArchiveInterpreterProperty(
+        propName,
+        propValue,
+        property,
+        validationContext,
+      );
+    case 'CellWriter':
+      return checkCellWriterProperty(
+        propName,
+        property,
+        validationContext,
+        evaluationContext,
+      );
+    case 'ColumnDeleter':
+      return checkColumnDeleterProperty(
+        propName,
+        property,
+        validationContext,
+        evaluationContext,
+      );
+    case 'GtfsRTInterpreter':
+      return checkGtfsRTInterpreterProperty(
+        propName,
+        propValue,
+        property,
+        validationContext,
+      );
+    case 'HttpExtractor':
+      return checkHttpExtractorProperty(
+        propName,
+        propValue,
+        property,
+        validationContext,
+      );
+    case 'RowDeleter':
+      return checkRowDeleterProperty(
+        propName,
+        property,
+        validationContext,
+        evaluationContext,
+      );
+    case 'TableInterpreter':
+      return checkTableInterpreterProperty(
+        propName,
+        property,
+        validationContext,
+        evaluationContext,
+      );
+    case 'TextFileInterpreter':
+      return checkTextFileInterpreterProperty(
+        propName,
+        propValue,
+        property,
+        validationContext,
+      );
+    case 'TextLineDeleter':
+      return checkTextLineDeleterProperty(
+        propName,
+        property,
+        validationContext,
+        evaluationContext,
+      );
+    case 'TextRangeSelector':
+      return checkTextRangeSelectorProperty(
+        propName,
+        propValue,
+        property,
+        validationContext,
+      );
+    default:
+  }
+}
+
+function checkArchiveInterpreterProperty(
+  propName: string,
+  propValue: InternalValueRepresentation,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+) {
+  const allowedArchiveTypes: InternalValueRepresentation[] = ['zip', 'gz'];
+  if (propName === 'archiveType') {
+    if (!allowedArchiveTypes.includes(propValue)) {
+      validationContext.accept(
+        'error',
+        `The value of property "${propName}" must be one of the following values: ${allowedArchiveTypes
+          .map((v) => `"${v.toString()}"`)
+          .join(', ')}`,
+        {
+          node: property,
+          property: 'value',
+        },
+      );
+    }
+  }
+}
+
+function checkCellWriterProperty(
+  propName: string,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+  evaluationContext: EvaluationContext,
+) {
+  if (propName === 'at') {
+    const cellRange = evaluatePropertyValue(
+      property,
+      evaluationContext,
+      PrimitiveValuetypes.CellRange,
+    );
+    if (cellRange === undefined) {
+      return;
+    }
+
+    if (!cellRange.isOneDimensional()) {
+      validationContext.accept(
+        'error',
+        'The cell range needs to be one-dimensional',
+        {
+          node: cellRange.astNode,
+        },
+      );
+    }
+  }
+}
+
+function checkColumnDeleterProperty(
+  propName: string,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+  evaluationContext: EvaluationContext,
+) {
+  if (propName === 'delete') {
+    const cellRanges = evaluatePropertyValue(
+      property,
+      evaluationContext,
+      new CollectionValuetype(PrimitiveValuetypes.CellRange),
+    );
+
+    cellRanges?.forEach((cellRange) => {
+      if (!isColumnWrapper(cellRange)) {
+        validationContext.accept(
+          'error',
+          'An entire column needs to be selected',
+          {
+            node: cellRange.astNode,
+          },
+        );
+      }
+    });
+  }
+}
+
+function checkGtfsRTInterpreterProperty(
+  propName: string,
+  propValue: InternalValueRepresentation,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+) {
+  const allowedEntities: InternalValueRepresentation[] = [
+    'trip_update',
+    'alert',
+    'vehicle',
+  ];
+  if (propName === 'entity') {
+    if (!allowedEntities.includes(propValue)) {
+      validationContext.accept(
+        'error',
+        `The value of property "${propName}" must be one of the following values: ${allowedEntities
+          .map((v) => `"${v.toString()}"`)
+          .join(', ')}`,
+        {
+          node: property,
+          property: 'value',
+        },
+      );
+    }
+  }
+}
+
+function checkHttpExtractorProperty(
+  propName: string,
+  propValue: InternalValueRepresentation,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+) {
+  if (propName === 'retries') {
+    const minRetryValue = 0;
+    if (typeof propValue === 'number' && propValue < minRetryValue) {
+      validationContext.accept(
+        'error',
+        `The value of property "${propName}" must not be smaller than ${minRetryValue}`,
+        {
+          node: property,
+          property: 'value',
+        },
+      );
+    }
+  }
+  if (propName === 'retryBackoffMilliseconds') {
+    const minBackoffValue = 1000;
+    if (typeof propValue === 'number' && propValue < minBackoffValue) {
+      validationContext.accept(
+        'error',
+        `The value of property "${propName}" must not be smaller than ${minBackoffValue}`,
+        {
+          node: property,
+          property: 'value',
+        },
+      );
+    }
+  }
+  if (propName === 'retryBackoffStrategy') {
+    const allowedRetryStrategies: InternalValueRepresentation[] = [
+      'exponential',
+      'linear',
+    ];
+    if (!allowedRetryStrategies.includes(propValue)) {
+      validationContext.accept(
+        'error',
+        `The value of property "${propName}" must be one of the following values: ${allowedRetryStrategies
+          .map((v) => `"${v.toString()}"`)
+          .join(', ')}`,
+        {
+          node: property,
+          property: 'value',
+        },
+      );
+    }
+  }
+}
+
+function checkRowDeleterProperty(
+  propName: string,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+  evaluationContext: EvaluationContext,
+) {
+  if (propName === 'delete') {
+    const cellRanges = evaluatePropertyValue(
+      property,
+      evaluationContext,
+      new CollectionValuetype(PrimitiveValuetypes.CellRange),
+    );
+
+    cellRanges?.forEach((cellRange) => {
+      if (!isRowWrapper(cellRange)) {
+        validationContext.accept(
+          'error',
+          'An entire row needs to be selected',
+          {
+            node: cellRange.astNode,
+          },
+        );
+      }
+    });
+  }
+}
+
+function checkTableInterpreterProperty(
+  propName: string,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+  evaluationContext: EvaluationContext,
+) {
+  if (propName === 'columns') {
+    const valuetypeAssignments = evaluatePropertyValue(
+      property,
+      evaluationContext,
+      new CollectionValuetype(PrimitiveValuetypes.ValuetypeAssignment),
+    );
+    if (valuetypeAssignments === undefined) {
+      return;
+    }
+
+    checkUniqueNames(valuetypeAssignments, validationContext, 'column');
+  }
+}
+
+function checkTextFileInterpreterProperty(
+  propName: string,
+  propValue: InternalValueRepresentation,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Encoding_API/Encodings
+  const allowedEncodings: InternalValueRepresentation[] = [
+    'utf8',
+    'ibm866',
+    'latin2',
+    'latin3',
+    'latin4',
+    'cyrillic',
+    'arabic',
+    'greek',
+    'hebrew',
+    'logical',
+    'latin6',
+    'utf-16',
+  ];
+  if (propName === 'encoding') {
+    if (!allowedEncodings.includes(propValue)) {
+      validationContext.accept(
+        'error',
+        `The value of property "${propName}" must be one of the following values: ${allowedEncodings
+          .map((v) => `"${v.toString()}"`)
+          .join(', ')}`,
+        {
+          node: property,
+          property: 'value',
+        },
+      );
+    }
+  }
+}
+
+function checkTextLineDeleterProperty(
+  propName: string,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+  evaluationContext: EvaluationContext,
+) {
+  if (propName === 'lines') {
+    const minTextLineIndex = 1;
+    const lines = evaluatePropertyValue(
+      property,
+      evaluationContext,
+      new CollectionValuetype(PrimitiveValuetypes.Integer),
+    );
+    lines?.forEach((value, index) => {
+      if (value < minTextLineIndex) {
+        validationContext.accept(
+          'error',
+          `Line numbers need to be greater than zero`,
+          {
+            node: property.value,
+            property: 'values',
+            index: index,
+          },
+        );
+      }
+    });
+  }
+}
+
+function checkTextRangeSelectorProperty(
+  propName: string,
+  propValue: InternalValueRepresentation,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+) {
+  const minLineIndex = 1;
+  if (propName === 'lineFrom' || propName === 'lineTo') {
+    if (typeof propValue === 'number' && propValue < minLineIndex) {
+      validationContext.accept(
+        'error',
+        `The value of property "${propName}" must not be smaller than ${minLineIndex}`,
+        {
+          node: property,
+          property: 'value',
+        },
+      );
+    }
+  }
+}
