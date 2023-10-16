550136c3cb03e0bae0662feb960f227b64a3ddd8
Felix Müller <felix.mueller0998@gmx.de>
2023-10-16 11:25:50 +0200

    Added TextRangeSelectorExecutor tests

---
+++ libs/extensions/std/exec/src/text-range-selector-executor.spec.ts
@@ -0,0 +1,108 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import * as path from 'path';
+
+import * as R from '@jvalue/jayvee-execution';
+import { getTestExecutionContext } from '@jvalue/jayvee-execution/test';
+import { StdLangExtension } from '@jvalue/jayvee-extensions/std/lang';
+import {
+  BlockDefinition,
+  IOType,
+  createJayveeServices,
+  useExtension,
+} from '@jvalue/jayvee-language-server';
+import {
+  ParseHelperOptions,
+  TestLangExtension,
+  expectNoParserAndLexerErrors,
+  parseHelper,
+  readJvTestAssetHelper,
+} from '@jvalue/jayvee-language-server/test';
+import { AstNode, AstNodeLocator, LangiumDocument } from 'langium';
+import { NodeFileSystem } from 'langium/node';
+
+import { createTextFileFromLocalFile } from '../test';
+
+import { TextRangeSelectorExecutor } from './text-range-selector-executor';
+
+describe('Validation of TextRangeSelectorExecutor', () => {
+  let parse: (
+    input: string,
+    options?: ParseHelperOptions,
+  ) => Promise<LangiumDocument<AstNode>>;
+
+  let locator: AstNodeLocator;
+
+  const readJvTestAsset = readJvTestAssetHelper(
+    __dirname,
+    '../test/assets/text-range-selector-executor/',
+  );
+
+  function readTestFile(fileName: string): R.TextFile {
+    const absoluteFileName = path.resolve(
+      __dirname,
+      '../test/assets/text-range-selector-executor/',
+      fileName,
+    );
+    return createTextFileFromLocalFile(absoluteFileName);
+  }
+
+  async function parseAndExecuteExecutor(
+    input: string,
+    IOInput: R.TextFile,
+  ): Promise<R.Result<R.TextFile>> {
+    const document = await parse(input, { validationChecks: 'all' });
+    expectNoParserAndLexerErrors(document);
+
+    const block = locator.getAstNode<BlockDefinition>(
+      document.parseResult.value,
+      'pipelines@0/blocks@1',
+    ) as BlockDefinition;
+
+    return new TextRangeSelectorExecutor().doExecute(
+      IOInput,
+      getTestExecutionContext(locator, document, [block]),
+    );
+  }
+
+  beforeAll(() => {
+    // Register extensions
+    useExtension(new StdLangExtension());
+    useExtension(new TestLangExtension());
+    // Create language services
+    const services = createJayveeServices(NodeFileSystem).Jayvee;
+    locator = services.workspace.AstNodeLocator;
+    // Parse function for Jayvee (without validation)
+    parse = parseHelper(services);
+  });
+
+  it('should diagnose no error on valid file', async () => {
+    const text = readJvTestAsset('valid-range.jv');
+
+    const testFile = readTestFile('test.txt');
+    const result = await parseAndExecuteExecutor(text, testFile);
+
+    expect(R.isErr(result)).toEqual(false);
+    if (R.isOk(result)) {
+      expect(result.right.ioType).toEqual(IOType.TEXT_FILE);
+      expect(result.right.content).toEqual(
+        expect.arrayContaining(['Multiline', 'Test ']),
+      );
+    }
+  });
+
+  it('should diagnose no error on empty text file', async () => {
+    const text = readJvTestAsset('valid-range.jv');
+
+    const testFile = readTestFile('test-empty.txt');
+    const result = await parseAndExecuteExecutor(text, testFile);
+
+    expect(R.isErr(result)).toEqual(false);
+    if (R.isOk(result)) {
+      expect(result.right.ioType).toEqual(IOType.TEXT_FILE);
+      expect(result.right.content).toHaveLength(0);
+    }
+  });
+});
