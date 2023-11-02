02b6acf0ac6d80aa403a1cf8ee3bd14a5f94489b
Georg Schwarz <schwarz@group.riehle.org>
2023-11-02 18:48:16 +0100

    Re-add validations on constraint property assignment

---
+++ libs/language-server/src/lib/validation/checks/constrainttype-specific/property-assignment.ts
@@ -0,0 +1,79 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import {
+  EvaluationContext,
+  PrimitiveValuetypes,
+  PropertyAssignment,
+  evaluatePropertyValue,
+} from '../../../ast';
+import { PropertySpecification } from '../../../meta-information';
+import { ValidationContext } from '../../validation-context';
+
+export function checkConstraintTypeSpecificProperties(
+  property: PropertyAssignment,
+  propertySpec: PropertySpecification,
+  validationContext: ValidationContext,
+  evaluationContext: EvaluationContext,
+) {
+  const propName = property.name;
+  const propValue = evaluatePropertyValue(
+    property,
+    evaluationContext,
+    propertySpec.type,
+  );
+  if (propValue === undefined) {
+    return;
+  }
+
+  switch (property.$container.$container.type.ref?.name) {
+    case 'LengthConstraint':
+      return checkLengthConstraintProperty(
+        propName,
+        property,
+        validationContext,
+        evaluationContext,
+      );
+    default:
+  }
+}
+
+function checkLengthConstraintProperty(
+  propName: string,
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+  evaluationContext: EvaluationContext,
+) {
+  if (propName === 'minLength') {
+    checkNonNegative(property, validationContext, evaluationContext);
+  }
+  if (propName === 'maxLength') {
+    checkNonNegative(property, validationContext, evaluationContext);
+  }
+}
+
+function checkNonNegative(
+  property: PropertyAssignment,
+  validationContext: ValidationContext,
+  evaluationContext: EvaluationContext,
+) {
+  const value = evaluatePropertyValue(
+    property,
+    evaluationContext,
+    PrimitiveValuetypes.Integer,
+  );
+  if (value === undefined) {
+    return;
+  }
+
+  if (value < 0) {
+    validationContext.accept(
+      'error',
+      `Bounds for "${property.name}" need to be equal or greater than zero`,
+      {
+        node: property.value,
+      },
+    );
+  }
+}
