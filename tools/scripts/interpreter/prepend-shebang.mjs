de18e1f005dc04466ed6df71db8bad57334e24f8
Georg Schwarz <schwarz@group.riehle.org>
2023-11-08 13:53:20 +0100

    Execute all commands in nx jobs sequentially

--- tools/scripts/interpreter/prepend-shebang.mjs
+++ tools/scripts/interpreter/prepend-shebang.mjs
@@ -2,8 +2,8 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import {getOutputPath} from "../shared-util.mjs";
-import fs from "fs";
+import { getOutputPath } from '../shared-util.mjs';
+import fs from 'fs';
 
 // Executing this script: node path/to/prepend-shebang.mjs {projectName} {file}
 const [, , projectName, file] = process.argv;
@@ -11,5 +11,7 @@ const shebang = '#!/usr/bin/env node';
 
 process.chdir(getOutputPath(projectName));
 
-const previousFileContent = fs.readFileSync(file)
+console.log(`Prepending shebang to file ${process.cwd()}/${file}`);
+const previousFileContent = fs.readFileSync(file);
 fs.writeFileSync(file, `${shebang}\n${previousFileContent}`);
+console.log(`Finished appending shebang!`);
