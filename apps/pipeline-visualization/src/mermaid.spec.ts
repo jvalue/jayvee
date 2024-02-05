ad72cf077bdfe5c326f81811ac9ac2911e829244
Fabian Westphal <fabian.westphal@fau.de>
2024-02-05 16:52:00 +0100

    adjusted the tests

--- apps/pipeline-visualization/src/mermaid.spec.ts
+++ apps/pipeline-visualization/src/mermaid.spec.ts
@@ -5,7 +5,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-import { MermaidOptions } from './mermaid_utils';
+import { MermaidOptions } from './mermaid_builder';
 import { doProcessOptions } from './run';
 
 describe('test mermaid code generation for cars.jv', () => {
@@ -91,14 +91,14 @@ CarsExtractor-->CarsTextFileInterpreter-->CarsCSVInterpreter-->NameHeaderWriter-
 end
 
 CarsExtractor[CarsExtractor<br><i>HttpExtractor</i>]
-class CarsExtractor source;
 CarsTextFileInterpreter[CarsTextFileInterpreter<br><i>TextFileInterpreter</i>]
 CarsCSVInterpreter[CarsCSVInterpreter<br><i>CSVInterpreter</i>]
 NameHeaderWriter[NameHeaderWriter<br><i>CellWriter</i>]
 CarsTableInterpreter[CarsTableInterpreter<br><i>TableInterpreter</i>]
 CarsLoader[CarsLoader<br><i>SQLiteLoader</i>]
-class CarsLoader sink;
 
+class CarsExtractor source;
+class CarsLoader sink;
 classDef source fill:#FF9999,stroke:#333,stroke-width:2px;
 classDef sink fill:#BDFFA4,stroke:#333,stroke-width:2px;`;
 
@@ -107,18 +107,15 @@ subgraph CarsPipeline
 CarsExtractor-->CarsTextFileInterpreter-->CarsCSVInterpreter-->NameHeaderWriter-->CarsTableInterpreter-->CarsLoader
 end
 
-CarsExtractor[CarsExtractor<br><i>HttpExtractor</i><br>]
-class CarsExtractor source;
-CarsTextFileInterpreter[CarsTextFileInterpreter<br><i>TextFileInterpreter</i><br>]
-CarsCSVInterpreter[CarsCSVInterpreter<br><i>CSVInterpreter</i><br>enclosing: &quot;
-]
-NameHeaderWriter[NameHeaderWriter<br><i>CellWriter</i><br>]
-CarsTableInterpreter[CarsTableInterpreter<br><i>TableInterpreter</i><br>]
-CarsLoader[CarsLoader<br><i>SQLiteLoader</i><br>table: Cars
-file: ./cars.sqlite
-]
-class CarsLoader sink;
+CarsExtractor[CarsExtractor<br><i>HttpExtractor</i>]
+CarsTextFileInterpreter[CarsTextFileInterpreter<br><i>TextFileInterpreter</i>]
+CarsCSVInterpreter[CarsCSVInterpreter<br><i>CSVInterpreter</i><br>enclosing: &quot;]
+NameHeaderWriter[NameHeaderWriter<br><i>CellWriter</i>]
+CarsTableInterpreter[CarsTableInterpreter<br><i>TableInterpreter</i>]
+CarsLoader[CarsLoader<br><i>SQLiteLoader</i><br>table: Cars<br>file: ./cars.sqlite]
 
+class CarsExtractor source;
+class CarsLoader sink;
 classDef source fill:#FF9999,stroke:#333,stroke-width:2px;
 classDef sink fill:#BDFFA4,stroke:#333,stroke-width:2px;`;
 
@@ -128,11 +125,11 @@ CarsExtractor-->CarsTableInterpreter-->CarsLoader
 end
 
 CarsExtractor[CarsExtractor<br><i>CSVExtractor</i>]
-class CarsExtractor source;
 CarsLoader[CarsLoader<br><i>SQLiteLoader</i>]
-class CarsLoader sink;
 CarsTableInterpreter[CarsTableInterpreter<br><i>TableInterpreter</i>]
 
+class CarsExtractor source;
+class CarsLoader sink;
 classDef source fill:#FF9999,stroke:#333,stroke-width:2px;
 classDef sink fill:#BDFFA4,stroke:#333,stroke-width:2px;`;
 
@@ -146,14 +143,14 @@ CarsExtractor-->CarsTableInterpreter-->CarsLoader
 end
 
 CarsExtractor[CarsExtractor<br><i>CSVExtractor</i>]
-class CarsExtractor source;
 FileExtractor[FileExtractor<br><i>HttpExtractor</i>]
-class FileExtractor source;
 FileTextInterpreter[FileTextInterpreter<br><i>TextFileInterpreter</i>]
 FileCSVInterpreter[FileCSVInterpreter<br><i>CSVInterpreter</i>]
 CarsLoader[CarsLoader<br><i>SQLiteLoader</i>]
-class CarsLoader sink;
 CarsTableInterpreter[CarsTableInterpreter<br><i>TableInterpreter</i>]
 
+class CarsExtractor source;
+class FileExtractor source;
+class CarsLoader sink;
 classDef source fill:#FF9999,stroke:#333,stroke-width:2px;
 classDef sink fill:#BDFFA4,stroke:#333,stroke-width:2px;`;
