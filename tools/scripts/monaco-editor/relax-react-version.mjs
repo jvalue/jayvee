07e3adbae4f4d51ee0ff101b675f6b48b256ace2
Philip Heltweg <philip@heltweg.org>
2024-02-12 14:33:26 +0100

    Fixed publishing to npm failing because of peerDependencies (#528)
    * ci: :green_heart: Fixed publishing to npm failing because of peerDependencies
    
    * ci: :memo: Fixed wrong name for workflow run

--- tools/scripts/monaco-editor/relax-react-version.mjs
+++ tools/scripts/monaco-editor/relax-react-version.mjs
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import {getOutputPath, parsePackageJson, writePackageJson} from "../shared-util.mjs";
+import { getOutputPath, parsePackageJson, writePackageJson } from "../shared-util.mjs";
 
 // Executing this script: node path/to/relax-react-version.mjs {projectName}
 const [, , projectName] = process.argv;
@@ -11,6 +11,8 @@ process.chdir(getOutputPath(projectName));
 const packageJson = parsePackageJson();
 
 // By default, this value is set to the exact React version we are using. This makes it hard to use the package in environments where a different React version is present.
-packageJson.peerDependencies.react = '>= 17';
+if (packageJson.peerDependencies) {
+    packageJson.peerDependencies.react = '>= 17';
+}
 
 writePackageJson(packageJson);
