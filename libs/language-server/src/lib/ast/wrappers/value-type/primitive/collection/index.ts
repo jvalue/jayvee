aa6033755c4fbcb014c3d502023fdd8d0a07dd54
Georg Schwarz <schwarz@group.riehle.org>
2024-04-18 09:29:29 +0200

    Add reason to use of type exports in index.ts files in wrappers directory

--- libs/language-server/src/lib/ast/wrappers/value-type/primitive/collection/index.ts
+++ libs/language-server/src/lib/ast/wrappers/value-type/primitive/collection/index.ts
@@ -2,7 +2,10 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-/* Note: Only export types if possible to enforce usage of WrapperFactory outside this directory */
+/*
+ * Note: Only export types if possible to enforce usage of WrapperFactory outside this directory.
+ * This allows us to avoid dependency cycles between the language server and interpreter.
+ */
 
 export {
   type CollectionValueType,
