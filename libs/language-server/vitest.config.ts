bc63faefedfb15467c3b7d25d58fb12db210f93b
Georg Schwarz <schwarz@group.riehle.org>
2024-04-19 15:45:26 +0200

    Fix vitest.config.ts

--- libs/language-server/vitest.config.ts
+++ libs/language-server/vitest.config.ts
@@ -4,7 +4,7 @@
 
 // / <reference types='vitest' />
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   root: __dirname,
