503e59acea2315638b50b7fade02e252505dc261
Georg Schwarz <schwarz@group.riehle.org>
2024-05-28 11:44:11 +0200

    Add definition provider for explicitly imported elements

--- libs/language-server/src/lib/lsp/jayvee-definition-provider.ts
+++ libs/language-server/src/lib/lsp/jayvee-definition-provider.ts
@@ -2,7 +2,12 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
+// eslint-disable-next-line unicorn/prefer-node-protocol
+import { strict as assert } from 'assert';
+
 import {
+  type AstNode,
+  AstUtils,
   GrammarUtils,
   type LangiumDocuments,
   type LeafCstNode,
@@ -15,7 +20,7 @@ import {
   Range,
 } from 'vscode-languageserver-protocol';
 
-import { isImportDefinition } from '../ast';
+import { getExportedElements, isImportDefinition, isJayveeModel } from '../ast';
 import { type JayveeServices } from '../jayvee-module';
 import { type JayveeImportResolver } from '../services/import-resolver';
 
@@ -34,6 +39,7 @@ export class JayveeDefinitionProvider extends DefaultDefinitionProvider {
     params: DefinitionParams,
   ): MaybePromise<LocationLink[] | undefined> {
     const sourceAstNode = sourceCstNode.astNode;
+
     if (
       isImportDefinition(sourceAstNode) &&
       GrammarUtils.findAssignment(sourceCstNode)?.feature === 'path'
@@ -44,23 +50,76 @@ export class JayveeDefinitionProvider extends DefaultDefinitionProvider {
         return undefined;
       }
 
-      const jumpTarget = importedModel;
+      return this.getLocationLink(sourceCstNode, importedModel);
+    }
 
-      const selectionRange =
-        this.nameProvider.getNameNode(jumpTarget)?.range ??
-        Range.create(0, 0, 0, 0);
-      const previewRange =
-        jumpTarget.$cstNode?.range ?? Range.create(0, 0, 0, 0);
+    if (
+      isImportDefinition(sourceAstNode) &&
+      GrammarUtils.findAssignment(sourceCstNode)?.feature === 'usedElements'
+    ) {
+      const clickedIndex =
+        GrammarUtils.findAssignment(sourceCstNode)?.$container?.$containerIndex;
+      assert(
+        clickedIndex !== undefined,
+        'Could not read index of selected element',
+      );
+      const indexOfElement = clickedIndex - 1;
+      assert(
+        indexOfElement < sourceAstNode.usedElements.length,
+        'Index of selected element is not correctly computed',
+      );
+      const refString = sourceAstNode.usedElements[indexOfElement];
+      assert(
+        refString !== undefined,
+        'Could not read reference text to imported element',
+      );
 
-      return [
-        LocationLink.create(
-          importedModel.$document.uri.toString(),
-          previewRange,
-          selectionRange,
-          sourceCstNode.range,
-        ),
-      ];
+      const importedModel = this.importResolver.resolveImport(sourceAstNode);
+
+      if (importedModel?.$document === undefined) {
+        return undefined;
+      }
+
+      const allExportDefinitions = getExportedElements(importedModel);
+
+      const referencedExport = allExportDefinitions.find((x) => {
+        return x.alias === refString;
+      });
+      if (referencedExport === undefined) {
+        return;
+      }
+
+      return this.getLocationLink(sourceCstNode, referencedExport.element);
     }
     return super.collectLocationLinks(sourceCstNode, params);
+  }
+
+  protected getLocationLink(
+    sourceCstNode: LeafCstNode,
+    jumpTarget: AstNode,
+  ): LocationLink[] | undefined {
+    // need to go over model as jumpTarget might not have $document associated
+    const containingDocument = AstUtils.getContainerOfType(
+      jumpTarget,
+      isJayveeModel,
+    )?.$document;
+
+    if (containingDocument === undefined) {
+      return undefined;
+    }
+
+    const selectionRange =
+      this.nameProvider.getNameNode(jumpTarget)?.range ??
+      Range.create(0, 0, 0, 0);
+    const previewRange = jumpTarget.$cstNode?.range ?? Range.create(0, 0, 0, 0);
+
+    return [
+      LocationLink.create(
+        containingDocument.uri.toString(),
+        previewRange,
+        selectionRange,
+        sourceCstNode.range,
+      ),
+    ];
   }
 }
