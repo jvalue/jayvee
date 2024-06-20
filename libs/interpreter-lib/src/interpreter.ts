c368a4e8918d6f7db445cf61f4f8df4e616e593e
Georg Schwarz <schwarz@group.riehle.org>
2024-06-20 12:54:19 +0200

    Add missing license headers

--- libs/interpreter-lib/src/interpreter.ts
+++ libs/interpreter-lib/src/interpreter.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 // eslint-disable-next-line no-restricted-imports
 import { strict as assert } from 'node:assert';
 
