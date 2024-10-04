fc6fc1b566c94133fb470fc02b932e611074dafa
Jonas Zeltner <jonas.zeltner@fau.de>
2024-10-04 15:19:38 +0200

    Add licence information

--- libs/monaco-editor/src/lib/monaco-editor.ts
+++ libs/monaco-editor/src/lib/monaco-editor.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 import tmGrammar from './jayvee.tmLanguage.json';
 import config from './language-configuration.json';
 
