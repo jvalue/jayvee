d29a504c92d0e6dadbefd2f237c0e9c608cf8966
Philip Heltweg <philip@heltweg.org>
2024-12-20 15:36:21 +0100

    fix: :bug: Fix failing license compliance checks

--- apps/docs-generator/src/UserDocCategoryBuilder.ts
+++ apps/docs-generator/src/UserDocCategoryBuilder.ts
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Friedrich-Alexander-Universitat Erlangen-Nurnberg
 //
 // SPDX-License-Identifier: AGPL-3.0-only
+
 import * as fs from 'node:fs';
 import path from 'node:path';
 
