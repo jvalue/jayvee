58357ee999936f44d13f1118e027c1574a9f74b2
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-02-04 14:37:43 +0100

    Add tests for trimming whitespace

--- libs/extensions/tabular/exec/src/lib/table-interpreter-executor.spec.ts
+++ libs/extensions/tabular/exec/src/lib/table-interpreter-executor.spec.ts
@@ -299,5 +299,57 @@ describe('Validation of TableInterpreterExecutor', () => {
         expect(result.right.getNumberOfRows()).toEqual(0);
       }
     });
+
+    it('should skip leading and trailing whitespace on numeric columns but not text columns', async () => {
+      const text = readJvTestAsset('valid-without-header.jv');
+
+      const testWorkbook = await readTestWorkbook('test-with-whitespace.xlsx');
+      const result = await parseAndExecuteExecutor(
+        text,
+        testWorkbook.getSheetByName('Sheet1') as R.Sheet,
+      );
+
+      expect(R.isErr(result)).toEqual(false);
+      assert(R.isOk(result));
+
+      expect(result.right.ioType).toEqual(IOType.TABLE);
+      expect(result.right.getNumberOfColumns()).toEqual(3);
+      expect(result.right.getNumberOfRows()).toEqual(3);
+
+      expect([...result.right.getColumns().keys()]).toStrictEqual([
+        'index',
+        'name',
+        'flag',
+      ]);
+
+      const row = result.right.getRow(0);
+      const index = row.get('index');
+      expect(index).toBe(0);
+      const name = row.get('name');
+      expect(name).toBe('      text with leading whitespace');
+
+      for (let rowIdx = 1; rowIdx < result.right.getNumberOfRows(); rowIdx++) {
+        const row = result.right.getRow(rowIdx);
+        const index = row.get('index');
+        expect(index).toBe(rowIdx);
+      }
+    });
+
+    it('should not skip leading or trailing whitespace if the relevant block properties are false', async () => {
+      const text = readJvTestAsset('valid-without-header-without-trim.jv');
+
+      const testWorkbook = await readTestWorkbook('test-with-whitespace.xlsx');
+      const result = await parseAndExecuteExecutor(
+        text,
+        testWorkbook.getSheetByName('Sheet1') as R.Sheet,
+      );
+
+      expect(R.isErr(result)).toEqual(false);
+      if (R.isOk(result)) {
+        expect(result.right.ioType).toEqual(IOType.TABLE);
+        expect(result.right.getNumberOfColumns()).toEqual(3);
+        expect(result.right.getNumberOfRows()).toEqual(0);
+      }
+    });
   });
 });
