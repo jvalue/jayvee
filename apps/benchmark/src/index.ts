938671d7306944881cf2b9632b629e2da199b424
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-02-20 18:13:30 +0100

    Add licensing information

--- apps/benchmark/src/index.ts
+++ apps/benchmark/src/index.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 // eslint-disable-next-line unicorn/prefer-node-protocol
 import assert from 'assert';
 import path from 'node:path';
