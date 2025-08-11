7086d3177ffceb0452e39a7770ab48cf1beae8e9
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-08-11 09:56:54 +0200

    chore: upgrate eslint to latest

---
+++ apps/docs-generator/eslint.config.mjs
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2025 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+  ...baseConfig,
+  {
+    ignores: ['!**/*'],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+];
---
+++ apps/vs-code-extension/eslint.config.mjs
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+  ...baseConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+];
