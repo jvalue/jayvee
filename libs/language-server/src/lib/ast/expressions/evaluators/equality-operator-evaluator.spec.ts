789f30dfe145f38ec1a6a539162c711fdbdaa8ff
Jonas Zeltner <jonas.zeltner@posteo.de>
2025-09-14 12:53:58 +0200

    fix(comparison-operators): correct comparison for error values

---
+++ libs/language-server/src/lib/ast/expressions/evaluators/equality-operator-evaluator.spec.ts
@@ -0,0 +1,46 @@
+// SPDX-FileCopyrightText: 2025 Friedrich-Alexander-Universitat Erlangen-Nurnberg
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+import {
+  InvalidValue,
+  MissingValue,
+  type InternalErrorValueRepresentation,
+} from '../internal-value-representation';
+import { executeExpressionTestHelper } from '../test-utils';
+
+async function executeErrorValueComparison(
+  a: InternalErrorValueRepresentation,
+  b: 'invalid' | 'missing',
+) {
+  return executeExpressionTestHelper(
+    `inputValue == ${b}`,
+    'inputValue',
+    'text',
+    a,
+    'boolean',
+  );
+}
+
+describe('The equality operator', () => {
+  it('should compare invalid values correctly', async () => {
+    let result = await executeErrorValueComparison(
+      new InvalidValue(''),
+      'invalid',
+    );
+    expect(result).toBe(true);
+
+    result = await executeErrorValueComparison(new MissingValue(''), 'invalid');
+    expect(result).toBe(false);
+  });
+
+  it('should compare missing values correctly', async () => {
+    let result = await executeErrorValueComparison(
+      new MissingValue(''),
+      'missing',
+    );
+    expect(result).toBe(true);
+
+    result = await executeErrorValueComparison(new InvalidValue(''), 'missing');
+    expect(result).toBe(false);
+  });
+});
