d32d6d94d01ca8c5abd9e9baf77e0431cf2df34b
Jonas Zeltner <jonas.zeltner@posteo.de>
2026-03-05 09:14:24 +0100

    fix(docs): add documentation for `SheetRow`
    Problem: Document generation crashes because `SheetRow` does not provide
    required documentation
    
    Solution: Override the required method and provide a custom example
    
    The previous commits make sure the pipeline actually crashes at the correct
    location, instead of failing silently or too late.
    This commit actually fixes the pipeline

--- libs/language-server/src/lib/ast/wrappers/value-type/primitive/sheetrow-value-type.ts
+++ libs/language-server/src/lib/ast/wrappers/value-type/primitive/sheetrow-value-type.ts
@@ -5,6 +5,7 @@
 import { PrimitiveValueType } from './primitive-value-type';
 import { type ValueTypeVisitor } from '../value-type';
 import { type InternalValidValueRepresentation } from '../../../expressions';
+import { type ExampleDoc } from '../../typed-object/typed-object-wrapper';
 
 export class SheetRowValueType extends PrimitiveValueType<string[]> {
   acceptVisitor<R>(visitor: ValueTypeVisitor<R>): R {
@@ -17,6 +18,41 @@ export class SheetRowValueType extends PrimitiveValueType<string[]> {
 
   override getName(): 'SheetRow' {
     return 'SheetRow';
+  }
+
+  override getUserDocDescription(): string {
+    return `
+The values of a row inside a sheet. Only usable inside a transform that parses SheetRows into a value type.
+Accessible via the dot operator (see example).
+`.trim();
+  }
+
+  override getUserDocExamples(): ExampleDoc[] {
+    return [
+      {
+        code: `
+valuetype Coordinate {
+  x: decimal;
+  y: decimal;
+}
+transfrom coordinateParser {
+  from r oftype SheetRow;
+  to coord oftye Coordinate;
+
+  coord: {
+    x: asDecimal (r . "x");
+    y: asDecimal (r . 1);
+  };
+}
+block ExampleTableInterpreter oftype TableInterpreter {
+  header: true;
+  columns: Coordinate;
+  parseWith: CoordinateParser;
+}`.trim(),
+        description:
+          'A transform, used in a block of type `TableInterpreter`, that interprets a `SheetRow` into a table row with columns `x` and `y`.',
+      },
+    ];
   }
 
   override isInternalValidValueRepresentation(
